0c9d03c761e8a148272ce17acb976bcff516537d
Mark Todd <ddotkram@gmail.com>
2012-11-08 15:40:21 -0500

    Fixes to update origin in TreeWalkerVisito

--- src/main/java/edu/clemson/cs/r2jt/treewalk/TreeWalkerVisitor.java
+++ src/main/java/edu/clemson/cs/r2jt/treewalk/TreeWalkerVisitor.java
@@ -94,11 +94,8 @@ public abstract class TreeWalkerVisitor {
 
     public void midBetweenExpLessExps(BetweenExp node, Exp previous, Exp next) {}
 
-<<<<<<< HEAD
     public void postBetweenExpLessExps(BetweenExp data) {}
 
-=======
->>>>>>> bccfdf2611d50e588674d91bacaba687f1517121
     // BooleanTy 
     public void preBooleanTy(BooleanTy data) {}
 
