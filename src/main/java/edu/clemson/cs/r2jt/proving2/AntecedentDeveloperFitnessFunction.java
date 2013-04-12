ecba6fde685e8df5bc44aabc8300013ec089b650
Hampton Smith <hampton.w.smith@gmail.com>
2013-04-12 19:02:45 -0400

    Fixed an error that caused expressions containing top-level conjuncts to be entered into the prover model as a single conjunct rather than being split up.

---
+++ src/main/java/edu/clemson/cs/r2jt/proving2/AntecedentDeveloperFitnessFunction.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.clemson.cs.r2jt.proving2;
+
+import edu.clemson.cs.r2jt.proving.absyn.PExp;
+import edu.clemson.cs.r2jt.proving2.model.Conjunct;
+import edu.clemson.cs.r2jt.proving2.model.PerVCProverModel;
+import edu.clemson.cs.r2jt.proving2.transformations.ExpandAntecedentBySubstitution;
+import edu.clemson.cs.r2jt.proving2.transformations.StrengthenConsequent;
+import edu.clemson.cs.r2jt.proving2.transformations.SubstituteInPlaceInConsequent;
+import edu.clemson.cs.r2jt.proving2.transformations.Transformation;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author hamptos
+ */
+public class AntecedentDeveloperFitnessFunction 
+        implements FitnessFunction<Transformation> {
+    
+    private Set<String> myConsequentVariableNames = new HashSet<String>();
+
+    public AntecedentDeveloperFitnessFunction(PerVCProverModel model) {
+        for (Conjunct c : model.getConsequentList()) {
+            myConsequentVariableNames
+                    .addAll(c.getExpression().getSymbolNames());
+        }
+    }
+
+    @Override
+    public double calculateFitness(Transformation t) {
+        double result = 0;
+
+        if (t.couldAffectConsequent() || t.introducesQuantifiedVariables()) {
+            result = -1;
+        }
+        else if (t instanceof ExpandAntecedentBySubstitution) {
+            ExpandAntecedentBySubstitution tAsSIPIC =
+                    (ExpandAntecedentBySubstitution) t;
+
+            PExp pattern = tAsSIPIC.getMatchPattern();
+            PExp replacement = tAsSIPIC.getTransformationTemplate();
+            if (pattern.getFunctionApplications().isEmpty()
+                    && pattern.getQuantifiedVariables().size() == 1
+                    && replacement.getQuantifiedVariables().contains(
+                            pattern.getQuantifiedVariables().iterator().next())) {
+                result = -1;
+            }
+        }
+
+        return result;
+    }
+
+    private double unitAtan(int i) {
+        return (Math.atan(i) * 2 / Math.PI + 1) / 2;
+    }
+}
