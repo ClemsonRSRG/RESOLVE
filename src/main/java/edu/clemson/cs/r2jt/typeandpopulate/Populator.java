07431227bf3a930434e6982f1d46827fc904c594
Yu-Shan Sun <yushans@g.clemson.edu>
2015-04-16 11:07:08 -0400

    decreasing clause now must be of type N

--- src/main/java/edu/clemson/cs/r2jt/typeandpopulate/Populator.java
+++ src/main/java/edu/clemson/cs/r2jt/typeandpopulate/Populator.java
@@ -624,13 +624,28 @@ public class Populator extends TreeWalkerVisitor {
             }
 
             if (!curOpParam.getName().equals(curProcParam.getName())) {
-                throw new SourceErrorException("Parmeter name does not "
+                throw new SourceErrorException("Parameter name does not "
                         + "match corresponding operation parameter name."
                         + "\n\nExpected name: " + curOpParam.getName() + " ("
                         + curOpParam.getSourceModuleIdentifier() + "."
                         + myCorrespondingOperation.getName() + ")\n\n"
                         + "Found name: " + curProcParam.getName(), curProcParam
                         .getDefiningElement().getLocation());
+            }
+        }
+
+        // If there is a decreasing clause, check to see if it is an "N"
+        if (dec.getRecursive()) {
+            MathSymbolEntry intendedEntry =
+                    getIntendedEntry(null, "N", dec.getDecreasing());
+            try {
+                if (!dec.getDecreasing().getMathType().isSubtypeOf(
+                        intendedEntry.getTypeValue())) {
+                    expected(dec.getDecreasing(), intendedEntry.getTypeValue());
+                }
+            }
+            catch (SymbolNotOfKindTypeException e) {
+                notAType(intendedEntry, dec.getLocation());
             }
         }
 
