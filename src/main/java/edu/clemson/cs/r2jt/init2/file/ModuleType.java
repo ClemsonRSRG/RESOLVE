7d2b5314eca15ff2c78c4004478277f05f9e40d3
Yu-Shan Sun <yushans@g.clemson.edu>
2015-05-20 09:07:49 -0400

    Comments

--- src/main/java/edu/clemson/cs/r2jt/init2/file/ModuleType.java
+++ src/main/java/edu/clemson/cs/r2jt/init2/file/ModuleType.java
@@ -12,15 +12,39 @@
  */
 package edu.clemson.cs.r2jt.init2.file;
 
-/** Provides access to type checkable module types. */
+/** <p>Provides access to type checkable module types.</p> */
 public class ModuleType {
 
     // ===========================================================
-    // Variables 
+    // Member Fields
     // ===========================================================
 
     private final String myName;
     private final String myExtension;
+
+    // ===========================================================
+    // Objects
+    // ===========================================================
+
+    /** <p>TODO: Description for this object.</p> */
+    public final static ModuleType THEORY = new ModuleType("Theory", "mt");
+
+    /** <p>TODO: Description for this object.</p> */
+    public final static ModuleType CONCEPT = new ModuleType("Concept", "co");
+
+    /** <p>TODO: Description for this object.</p> */
+    public final static ModuleType ENHANCEMENT =
+            new ModuleType("Enhancement", "en");
+
+    /** <p>TODO: Description for this object.</p> */
+    public final static ModuleType REALIZATION =
+            new ModuleType("Realization", "rb");
+
+    /** <p>TODO: Description for this object.</p> */
+    public final static ModuleType FACILITY = new ModuleType("Facility", "fa");
+
+    /** <p>TODO: Description for this object.</p> */
+    public final static ModuleType PROFILE = new ModuleType("Profile", "pp");
 
     // ===========================================================
     // Constructors
@@ -32,26 +56,23 @@ public class ModuleType {
     }
 
     // ===========================================================
-    // Objects
-    // ===========================================================
-
-    public final static ModuleType THEORY = new ModuleType("Theory", "mt");
-    public final static ModuleType CONCEPT = new ModuleType("Concept", "co");
-    public final static ModuleType ENHANCEMENT =
-            new ModuleType("Enhancement", "en");
-    public final static ModuleType REALIZATION =
-            new ModuleType("Realization", "rb");
-    public final static ModuleType FACILITY = new ModuleType("Facility", "fa");
-    public final static ModuleType PROFILE = new ModuleType("Profile", "pp");
-
-    // ===========================================================
     // Public Methods
     // ===========================================================
 
+    /**
+     * <p>TODO: Decription for this method.</p>
+     *
+     * @return
+     */
     public String getExtension() {
         return myExtension;
     }
 
+    /**
+     * <p>TODO: Decription for this method.</p>
+     *
+     * @return
+     */
     public String getName() {
         return myName;
     }
