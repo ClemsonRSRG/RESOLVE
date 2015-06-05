ef5473800af22426790a1288f1a2a2c693d2358b
Yu-Shan Sun <yushans@g.clemson.edu>
2015-06-05 19:47:29 -0400

    Added the missing decreasing clause check vc

--- src/main/java/edu/clemson/cs/r2jt/vcgeneration/VCGenerator.java
+++ src/main/java/edu/clemson/cs/r2jt/vcgeneration/VCGenerator.java
@@ -2155,6 +2155,45 @@ public class VCGenerator extends TreeWalkerVisitor {
             ensures = myTypeGraph.getTrueVarExp();
         }
 
+        // Check for recursive call of itself
+        if (myCurrentOperationEntry != null
+                && myCurrentOperationEntry.getName().equals(
+                        opDec.getName().getName())
+                && myCurrentOperationEntry.getReturnType() != null) {
+            // Create a new confirm statement using P_val and the decreasing clause
+            VarExp pVal =
+                    Utilities.createPValExp(myOperationDecreasingExp
+                            .getLocation(), myCurrentModuleScope);
+
+            // Create a new infix expression
+            IntegerExp oneExp = new IntegerExp();
+            oneExp.setValue(1);
+            oneExp.setMathType(myOperationDecreasingExp.getMathType());
+            InfixExp leftExp =
+                    new InfixExp(stmt.getLocation(), oneExp, Utilities
+                            .createPosSymbol("+"), Exp
+                            .copy(myOperationDecreasingExp));
+            leftExp.setMathType(myOperationDecreasingExp.getMathType());
+            InfixExp exp =
+                    Utilities.createLessThanEqExp(stmt.getLocation(), leftExp,
+                            pVal, BOOLEAN);
+
+            // Create the new confirm statement
+            Location loc;
+            if (myOperationDecreasingExp.getLocation() != null) {
+                loc = (Location) myOperationDecreasingExp.getLocation().clone();
+            }
+            else {
+                loc = (Location) stmt.getLocation().clone();
+            }
+            loc.setDetails("Show Termination of Recursive Call");
+            Utilities.setLocation(exp, loc);
+            ConfirmStmt conf = new ConfirmStmt(loc, exp, false);
+
+            // Add it to our list of assertions
+            myCurrentAssertiveCode.addCode(conf);
+        }
+
         // Get the requires clause for this operation
         Exp requires;
         boolean simplify = false;
