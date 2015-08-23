315c74dfd92fb8ed48e50a05b8344300784df680
Yu-Shan Sun <yushans@g.clemson.edu>
2015-08-23 19:17:05 -0400

    Added the statements

---
+++ src/main/java/edu/clemson/cs/rsrg/absyn/Statement.java
@@ -0,0 +1,52 @@
+/**
+ * Statement.java
+ * ---------------------------------
+ * Copyright (c) 2015
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.absyn;
+
+import edu.clemson.cs.rsrg.parsing.data.Location;
+
+/**
+ * <p>This is the abstract base class for all the statement
+ * intermediate objects that the compiler builds from the ANTLR4 AST tree.</p>
+ *
+ * @version 2.0
+ */
+public abstract class Statement extends ResolveConceptualElement {
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    /**
+     * <p>A helper constructor that allow us to store the location
+     * of the created object directly in the this class.</p>
+     *
+     * @param l A {@link Location} representation object.
+     */
+    protected Statement(Location l) {
+        super(l);
+    }
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    /**
+     * <p>This method must be implemented by all inherited classes
+     * to override the default clone method implementation.</p>
+     *
+     * @return A deep copy of the object.
+     */
+    @Override
+    public abstract Statement clone();
+
+}
