fa14c30240a12c4fc33d0c006b18add12bab818d
Yu-Shan Sun <yushans@g.clemson.edu>
2015-09-14 11:56:57 -0400

    Try to mirror the absyn package

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/absyn/statements/AssumeStmt.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/absyn/statements/AssumeStmt.java
@@ -10,7 +10,7 @@
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
-package edu.clemson.cs.rsrg.vcgeneration.absyn;
+package edu.clemson.cs.rsrg.vcgeneration.absyn.statements;
 
 import edu.clemson.cs.rsrg.absyn.Exp;
 import edu.clemson.cs.rsrg.absyn.Statement;
