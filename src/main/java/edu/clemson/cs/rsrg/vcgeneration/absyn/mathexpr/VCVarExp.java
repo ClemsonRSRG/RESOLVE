a0578469f2000440cd4a5555620a0acf4cc33dec
Yu-Shan Sun <yushans@g.clemson.edu>
2016-05-25 17:03:07 -0400

    Standarized javadoc for VC variable exps

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/absyn/mathexpr/VCVarExp.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/absyn/mathexpr/VCVarExp.java
@@ -23,7 +23,7 @@ import java.util.Map;
 
 /**
  * <p>This is the class serves as a wrapper for an {@link MathExp}.
- * When the {@link VCGenerator} creates new variable expresssions,
+ * When the {@link VCGenerator} creates new variable expressions,
  * it wraps the original expression inside this class.</p>
  *
  * @version 2.0
@@ -66,42 +66,23 @@ public class VCVarExp extends MathExp {
     // ===========================================================
 
     /**
-     * <p>This method creates a special indented
-     * text version of the class as a string.</p>
-     *
-     * @param indentSize The base indentation to the first line
-     *                   of the text.
-     * @param innerIndentSize The additional indentation increment
-     *                        for the subsequent lines.
-     *
-     * @return A formatted text string of the class.
+     * {@inheritDoc}
      */
     @Override
-    public String asString(int indentSize, int innerIndentSize) {
+    public final String asString(int indentSize, int innerIndentInc) {
         StringBuffer sb = new StringBuffer();
         printSpace(indentSize, sb);
-        sb.append("VCVarExp\n");
-
-        if (myOrigExp != null) {
-            printSpace(indentSize + innerIndentSize, sb);
-            sb.append("?");
-            sb.append(myOrigExp.asString(0, innerIndentSize));
-        }
+        sb.append("?");
+        sb.append(myOrigExp.asString(0, innerIndentInc));
 
         return sb.toString();
     }
 
     /**
-     * <p>This method attempts to find the provided expression in our
-     * subexpressions.</p>
-     *
-     * @param exp The expression we wish to locate.
-     *
-     * @return True if there is an instance of <code>exp</code>
-     * within this object's subexpressions. False otherwise.
+     * {@inheritDoc}
      */
     @Override
-    public boolean containsExp(Exp exp) {
+    public final boolean containsExp(Exp exp) {
         boolean found = false;
         if (myOrigExp != null) {
             found = myOrigExp.containsExp(exp);
@@ -111,18 +92,10 @@ public class VCVarExp extends MathExp {
     }
 
     /**
-     *  <p>This method attempts to find an expression with the given name in our
-     * subexpressions.</p>
-     *
-     * @param varName Expression name.
-     * @param IsOldExp Flag to indicate if the given name is of the form
-     *                 "#[varName]"
-     *
-     * @return True if there is a {@link Exp} within this object's
-     * subexpressions that matches <code>varName</code>. False otherwise.
+     * {@inheritDoc}
      */
     @Override
-    public boolean containsVar(String varName, boolean IsOldExp) {
+    public final boolean containsVar(String varName, boolean IsOldExp) {
         boolean found = false;
         if (myOrigExp != null) {
             if (IsOldExp) {
@@ -134,42 +107,28 @@ public class VCVarExp extends MathExp {
     }
 
     /**
-     * <p>This method overrides the default equals method implementation
-     * for the {@link VCVarExp} class.</p>
-     *
-     * @param o Object to be compared.
-     *
-     * @return True if all the fields are equal, false otherwise.
+     * {@inheritDoc}
      */
     @Override
-    public boolean equals(Object o) {
-        boolean result = false;
-        if (o instanceof VCVarExp) {
-            VCVarExp eAsVarExp = (VCVarExp) o;
-            result = myLoc.equals(eAsVarExp.myLoc);
+    public final boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        if (!super.equals(o))
+            return false;
 
-            if (result) {
-                result = myOrigExp.equals(eAsVarExp.myOrigExp);
-            }
-        }
+        VCVarExp vcVarExp = (VCVarExp) o;
 
-        return result;
+        return myOrigExp.equals(vcVarExp.myOrigExp);
+
     }
 
     /**
-     * <p>Shallow compare is too weak for many things, and equals() is too
-     * strict. This method returns <code>true</code> <strong>iff</code> this
-     * expression and the provided expression, <code>e</code>, are equivalent
-     * with respect to structure and all function and variable names.</p>
-     *
-     * @param e The expression to compare this one to.
-     *
-     * @return True <strong>iff</strong> this expression and the provided
-     *         expression are equivalent with respect to structure and all
-     *         function and variable names.
+     * {@inheritDoc}
      */
     @Override
-    public boolean equivalent(Exp e) {
+    public final boolean equivalent(Exp e) {
         boolean retval = false;
         if (e instanceof VCVarExp) {
             VCVarExp eAsVarExp = (VCVarExp) e;
@@ -180,25 +139,33 @@ public class VCVarExp extends MathExp {
     }
 
     /**
-     * <p>Returns a deep copy of this VC variable expression's actual expression.</p>
+     * <p>Returns this VC variable expression's actual expression.</p>
      *
      * @return The {@link Exp} that we prepended the "?" to.
      */
-    public Exp getExp() {
-        return myOrigExp.clone();
+    public final Exp getExp() {
+        return myOrigExp;
     }
 
     /**
-     * <p>This method returns the list of subexpressions.</p>
-     *
-     * @return A list containing subexpressions ({@link Exp}s).
+     * {@inheritDoc}
      */
     @Override
-    public List<Exp> getSubExpressions() {
+    public final List<Exp> getSubExpressions() {
         List<Exp> list = new ArrayList<>();
-        list.add(myOrigExp.clone());
+        list.add(myOrigExp);
 
         return list;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + myOrigExp.hashCode();
+        return result;
     }
 
     /**
@@ -209,18 +176,9 @@ public class VCVarExp extends MathExp {
      * @return The resulting {@link VCVarExp} from applying the remember rule.
      */
     @Override
-    public VCVarExp remember() {
+    public final VCVarExp remember() {
         return (VCVarExp) this.clone();
     }
-
-    /**
-     * <p>This method adds a new expression to our list of subexpressions.</p>
-     *
-     * @param index The index in our subexpression list.
-     * @param e The new {@link Exp} to be added.
-     */
-    // TODO: See the message in Exp.
-    //public void setSubExpression(int index, Exp e) {}
 
     /**
      * <p>This method applies the VC Generator's simplification step.</p>
@@ -228,22 +186,18 @@ public class VCVarExp extends MathExp {
      * @return The resulting {@link MathExp} from applying the simplification step.
      */
     @Override
-    public MathExp simplify() {
+    public final MathExp simplify() {
         return this.clone();
     }
 
     /**
-     * <p>Returns the expression in string format.</p>
-     *
-     * @return Expression as a string.
+     * {@inheritDoc}
      */
     @Override
-    public String toString() {
+    public final String toString() {
         StringBuffer sb = new StringBuffer();
-        if (myOrigExp != null) {
-            sb.append("?");
-            sb.append(myOrigExp.toString());
-        }
+        sb.append(myOrigExp.toString());
+        sb.append("'");
 
         return sb.toString();
     }
@@ -253,32 +207,18 @@ public class VCVarExp extends MathExp {
     // ===========================================================
 
     /**
-     * <p>Implemented by this concrete subclass of {@link Exp} to manufacture
-     * a copy of themselves.</p>
-     *
-     * @return A new {@link Exp} that is a deep copy of the original.
+     * {@inheritDoc}
      */
     @Override
-    protected Exp copy() {
+    protected final Exp copy() {
         return new VCVarExp(new Location(myLoc), myOrigExp.clone());
     }
 
     /**
-     * <p>Implemented by this concrete subclass of {@link Exp} to manufacture
-     * a copy of themselves where all subexpressions have been appropriately
-     * substituted. This class is assuming that <code>this</code>
-     * does not match any key in <code>substitutions</code> and thus need only
-     * concern itself with performing substitutions in its children.</p>
-     *
-     * @param substitutions A mapping from {@link Exp}s that should be
-     *                      substituted out to the {@link Exp} that should
-     *                      replace them.
-     *
-     * @return A new {@link Exp} that is a deep copy of the original with
-     *         the provided substitutions made.
+     * {@inheritDoc}
      */
     @Override
-    protected Exp substituteChildren(Map<Exp, Exp> substitutions) {
+    protected final Exp substituteChildren(Map<Exp, Exp> substitutions) {
         return new VCVarExp(new Location(myLoc), substitute(myOrigExp,
                 substitutions));
     }
