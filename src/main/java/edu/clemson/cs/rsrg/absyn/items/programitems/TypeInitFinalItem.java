ab2ebf64754dc0c17bea5e86a95685a7a5f5c13a
Yu-Shan Sun <yushans@g.clemson.edu>
2016-06-21 22:39:17 -0400

    Changed the type init/final items

--- src/main/java/edu/clemson/cs/rsrg/absyn/items/programitems/TypeInitFinalItem.java
+++ src/main/java/edu/clemson/cs/rsrg/absyn/items/programitems/TypeInitFinalItem.java
@@ -13,12 +13,10 @@
 package edu.clemson.cs.rsrg.absyn.items.programitems;
 
 import edu.clemson.cs.rsrg.absyn.clauses.AffectsClause;
-import edu.clemson.cs.rsrg.absyn.ResolveConceptualElement;
 import edu.clemson.cs.rsrg.absyn.statements.Statement;
 import edu.clemson.cs.rsrg.absyn.declarations.facilitydecl.FacilityDec;
 import edu.clemson.cs.rsrg.absyn.declarations.variabledecl.VarDec;
 import edu.clemson.cs.rsrg.parsing.data.Location;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -28,49 +26,7 @@ import java.util.List;
  *
  * @version 2.0
  */
-public class TypeInitFinalItem extends ResolveConceptualElement {
-
-    // ===========================================================
-    // ItemType
-    // ===========================================================
-
-    public enum ItemType {
-        INITIALIZATION {
-
-            @Override
-            public String toString() {
-                return "initialization";
-            }
-
-        },
-        FINALIZATION {
-
-            @Override
-            public String toString() {
-                return "finalization";
-            }
-
-        }
-    }
-
-    // ===========================================================
-    // Member Fields
-    // ===========================================================
-
-    /** <p>The affects clause.</p> */
-    private final AffectsClause myAffects;
-
-    /** <p>List of facility declarations.</p> */
-    private final List<FacilityDec> myFacilityDecs;
-
-    /** <p>The type of clause</p> */
-    private final ItemType myItemType;
-
-    /** <p>List of statements.</p> */
-    private final List<Statement> myStatements;
-
-    /** <p>List of variable declarations.</p> */
-    private final List<VarDec> myVariableDecs;
+public class TypeInitFinalItem extends AbstractTypeInitFinalItem {
 
     // ===========================================================
     // Constructors
@@ -91,12 +47,7 @@ public class TypeInitFinalItem extends ResolveConceptualElement {
     public TypeInitFinalItem(Location l, ItemType type, AffectsClause affects,
             List<FacilityDec> facilities, List<VarDec> variables,
             List<Statement> statements) {
-        super(l);
-        myAffects = affects;
-        myFacilityDecs = facilities;
-        myItemType = type;
-        myStatements = statements;
-        myVariableDecs = variables;
+        super(l, type, affects, facilities, variables, statements);
     }
 
     // ===========================================================
@@ -136,7 +87,7 @@ public class TypeInitFinalItem extends ResolveConceptualElement {
                     innerIndentInc));
         }
 
-        sb.append("end\n");
+        sb.append("end;\n");
 
         return sb.toString();
     }
@@ -155,139 +106,4 @@ public class TypeInitFinalItem extends ResolveConceptualElement {
                 newAffects, copyFacDecs(), copyVars(), copyStatements());
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        TypeInitFinalItem that = (TypeInitFinalItem) o;
-
-        if (myAffects != null ? !myAffects.equals(that.myAffects)
-                : that.myAffects != null)
-            return false;
-        if (!myFacilityDecs.equals(that.myFacilityDecs))
-            return false;
-        if (myItemType != that.myItemType)
-            return false;
-        if (!myStatements.equals(that.myStatements))
-            return false;
-        return myVariableDecs.equals(that.myVariableDecs);
-
-    }
-
-    /**
-     * <p>This method returns the affects clause
-     * in this type initialization/finalization block.</p>
-     *
-     * @return The {@link AffectsClause} representation object.
-     */
-    public final AffectsClause getAffectedVars() {
-        return myAffects;
-    }
-
-    /**
-     * <p>This method returns the list of facility declarations
-     * in this type initialization/finalization block.</p>
-     *
-     * @return A list of {@link FacilityDec} representation objects.
-     */
-    public final List<FacilityDec> getFacilities() {
-        return myFacilityDecs;
-    }
-
-    /**
-     * <p>This method returns the item type.</p>
-     *
-     * @return The {@link ItemType} object.
-     */
-    public final ItemType getItemType() {
-        return myItemType;
-    }
-
-    /**
-     * <p>This method returns the list of statements
-     * in this type initialization/finalization block.</p>
-     *
-     * @return A list of {@link Statement} representation objects.
-     */
-    public final List<Statement> getStatements() {
-        return myStatements;
-    }
-
-    /**
-     * <p>This method returns the list of variables
-     * in this type initialization/finalization block.</p>
-     *
-     * @return A list of {@link VarDec} representation objects.
-     */
-    public final List<VarDec> getVariables() {
-        return myVariableDecs;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final int hashCode() {
-        int result = myAffects != null ? myAffects.hashCode() : 0;
-        result = 31 * result + myFacilityDecs.hashCode();
-        result = 31 * result + myItemType.hashCode();
-        result = 31 * result + myStatements.hashCode();
-        result = 31 * result + myVariableDecs.hashCode();
-        return result;
-    }
-
-    // ===========================================================
-    // Private Methods
-    // ===========================================================
-
-    /**
-     * <p>This is a helper method that makes a copy of the
-     * list containing all the facility declarations.</p>
-     *
-     * @return A list containing {@link FacilityDec}s.
-     */
-    private List<FacilityDec> copyFacDecs() {
-        List<FacilityDec> copyArgs = new ArrayList<>();
-        for (FacilityDec f : myFacilityDecs) {
-            copyArgs.add((FacilityDec) f.clone());
-        }
-
-        return copyArgs;
-    }
-
-    /**
-     * <p>This is a helper method that makes a copy of the
-     * list containing all the statements.</p>
-     *
-     * @return A list containing {@link Statement}s.
-     */
-    private List<Statement> copyStatements() {
-        List<Statement> copyArgs = new ArrayList<>();
-        for (Statement s : myStatements) {
-            copyArgs.add(s.clone());
-        }
-
-        return copyArgs;
-    }
-
-    /**
-     * <p>This is a helper method that makes a copy of the
-     * list containing all the variable declarations.</p>
-     *
-     * @return A list containing {@link VarDec}s.
-     */
-    private List<VarDec> copyVars() {
-        List<VarDec> copyArgs = new ArrayList<>();
-        for (VarDec v : myVariableDecs) {
-            copyArgs.add((VarDec) v.clone());
-        }
-
-        return copyArgs;
-    }
 }
