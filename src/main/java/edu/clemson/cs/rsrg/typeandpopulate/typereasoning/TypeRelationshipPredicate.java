2c6a86301e78687dcf12ecc43b35954520cad2c0
Yu-Shan Sun <yushans@g.clemson.edu>
2016-06-25 10:18:52 -0400

    Refactored the type relationship predicate interface

---
+++ src/main/java/edu/clemson/cs/rsrg/typeandpopulate/typereasoning/TypeRelationshipPredicate.java
@@ -0,0 +1,51 @@
+/**
+ * TypeRelationshipPredicate.java
+ * ---------------------------------
+ * Copyright (c) 2016
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.typeandpopulate.typereasoning;
+
+import edu.clemson.cs.r2jt.typeandpopulate2.MTType;
+import edu.clemson.cs.rsrg.absyn.expressions.Exp;
+import java.util.Map;
+
+/**
+ * <p>This is an interface for all type relationship predicates
+ * that we might be able to establish using our type reasoning system.</p>
+ *
+ * @version 2.0
+ */
+public interface TypeRelationshipPredicate {
+
+    /**
+     *
+     * @param substitutions Map of substitutions for unbound variables.
+     *
+     * @return A {@code TypeRelationshipPredicate} after the substitution.
+     */
+    TypeRelationshipPredicate replaceUnboundVariablesInTypes(
+            Map<String, String> substitutions);
+
+    /**
+     * <p>Given two types, we attempt to statically demonstrate if
+     * their type relationship can be established statically.</p>
+     *
+     * @param canonical1 The first {@link MTType} object.
+     * @param canonical2 The second {@link MTType} object.
+     * @param typeBindings Map of established type bindings.
+     * @param expressionBindings Map of established expression bindings.
+     *
+     * @return {@code true} if it can be demonstrated statically, {@code false} otherwise.
+     */
+    boolean canBeDemonstratedStatically(MTType canonical1, MTType canonical2,
+            Map<String, MTType> typeBindings,
+            Map<String, Exp> expressionBindings);
+
+}
