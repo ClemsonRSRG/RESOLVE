312974c25d5c919dc0b9bdf7188f7eae5042b899
Yu-Shan Sun <yushans@g.clemson.edu>
2016-07-21 10:56:26 -0400

    Refactored NameAndEntryTypeQuery

--- src/main/java/edu/clemson/cs/rsrg/typeandpopulate/query/NameAndEntryTypeQuery.java
+++ src/main/java/edu/clemson/cs/rsrg/typeandpopulate/query/NameAndEntryTypeQuery.java
@@ -1,5 +1,5 @@
 /**
- * NameQuery.java
+ * NameAndEntryTypeQuery.java
  * ---------------------------------
  * Copyright (c) 2016
  * RESOLVE Software Research Group
@@ -14,87 +14,99 @@ package edu.clemson.cs.rsrg.typeandpopulate.query;
 
 import edu.clemson.cs.rsrg.parsing.data.PosSymbol;
 import edu.clemson.cs.rsrg.typeandpopulate.entry.SymbolTableEntry;
-import edu.clemson.cs.rsrg.typeandpopulate.query.searcher.NameSearcher;
+import edu.clemson.cs.rsrg.typeandpopulate.query.searcher.NameAndEntryTypeSearcher;
 import edu.clemson.cs.rsrg.typeandpopulate.query.searchpath.PossiblyQualifiedPath;
 import edu.clemson.cs.rsrg.typeandpopulate.symboltables.MathSymbolTable.FacilityStrategy;
 import edu.clemson.cs.rsrg.typeandpopulate.symboltables.MathSymbolTable.ImportStrategy;
 
 /**
- * <p>A <code>NameQuery</code> takes a (possibly-null) qualifier and a name
- * and searches for entries that match. If the qualifier is non-null, the
+ * <p>A <code>NameAndEntryQuery</code> takes a (possibly-null) qualifier, a
+ * name, and an entry type descended from <code>SymbolTableEntry</code>, and
+ * searched for entries that match, disregarding any entries with the correct
+ * name but incorrect type. If the qualifier is non-null, the
  * appropriate facility or module is searched. If it <em>is</em> null, a
  * search is performed using the provided <code>ImportStrategy</code> and
  * <code>FacilityStrategy</code>.</p>
  *
  * @version 2.0
  */
-public class NameQuery extends BaseMultimatchSymbolQuery<SymbolTableEntry> {
+public class NameAndEntryTypeQuery<T extends SymbolTableEntry>
+        extends
+            BaseMultimatchSymbolQuery<T> {
 
     // ===========================================================
     // Constructors
     // ===========================================================
 
     /**
-     * <p>This query searches for all entries that match the given name.</p>
+     * <p>This query searches for all entries that match the given name and
+     * has type <code>T</code>.</p>
      *
      * @param qualifier A qualifier symbol that indicates the instantiating
      *                  facility or module.
      * @param name Name of the entry to be searched.
+     * @param entryType The class type of the entry.
      * @param importStrategy The import strategy to use.
      * @param facilityStrategy The facility strategy to use.
      * @param localPriority Boolean flag that indicates whether or not
      *                      local items have priority.
      */
-    public NameQuery(PosSymbol qualifier, String name,
-            ImportStrategy importStrategy, FacilityStrategy facilityStrategy,
-            boolean localPriority) {
+    public NameAndEntryTypeQuery(PosSymbol qualifier, String name,
+            Class<T> entryType, ImportStrategy importStrategy,
+            FacilityStrategy facilityStrategy, boolean localPriority) {
         super(new PossiblyQualifiedPath(qualifier, importStrategy,
-                facilityStrategy, localPriority), new NameSearcher(name, false));
+                facilityStrategy, localPriority), new NameAndEntryTypeSearcher<>(name, entryType, false));
     }
 
     /**
      * <p>This query searches for all entries that match the given name
-     * represented as a {@link PosSymbol}.</p>
+     * represented as a {@link PosSymbol} and has type <code>T</code>.</p>
      *
      * @param qualifier A qualifier symbol that indicates the instantiating
      *                  facility or module.
      * @param name A name symbol of the entry to be searched.
+     * @param entryType The class type of the entry.
      * @param importStrategy The import strategy to use.
      * @param facilityStrategy The facility strategy to use.
      * @param localPriority Boolean flag that indicates whether or not
      *                      local items have priority.
      */
-    public NameQuery(PosSymbol qualifier, PosSymbol name,
-            ImportStrategy importStrategy, FacilityStrategy facilityStrategy,
-            boolean localPriority) {
-        this(qualifier, name.getName(), importStrategy, facilityStrategy,
-                localPriority);
+    public NameAndEntryTypeQuery(PosSymbol qualifier, PosSymbol name,
+            Class<T> entryType, ImportStrategy importStrategy,
+            FacilityStrategy facilityStrategy, boolean localPriority) {
+        this(qualifier, name.getName(), entryType, importStrategy,
+                facilityStrategy, localPriority);
     }
 
     /**
-     * <p>This query searches for all entries that match the given name
-     * using no import or facility strategies and no local priority.</p>
+     * <p>This query searches for all entries that match the given name and
+     * has type <code>T</code>, using no import or facility strategies and
+     * no local priority.</p>
      *
      * @param qualifier A qualifier symbol that indicates the instantiating
      *                  facility or module.
      * @param name Name of the entry to be searched.
+     * @param entryType The class type of the entry.
      */
-    public NameQuery(PosSymbol qualifier, String name) {
-        this(qualifier, name, ImportStrategy.IMPORT_NONE,
+    public NameAndEntryTypeQuery(PosSymbol qualifier, String name,
+            Class<T> entryType) {
+        this(qualifier, name, entryType, ImportStrategy.IMPORT_NONE,
                 FacilityStrategy.FACILITY_IGNORE, false);
     }
 
     /**
      * <p>This query searches for all entries that match the given name
-     * represented as a {@link PosSymbol} using no import or facility strategies
-     * and no local priority.</p>
+     * represented as a {@link PosSymbol} and has type <code>T</code>,
+     * using no import or facility strategies and no local priority.</p>
      *
      * @param qualifier A qualifier symbol that indicates the instantiating
      *                  facility or module.
      * @param name A name symbol of the entry to be searched.
+     * @param entryType The class type of the entry.
      */
-    public NameQuery(PosSymbol qualifier, PosSymbol name) {
-        this(qualifier, name, ImportStrategy.IMPORT_NONE,
+    public NameAndEntryTypeQuery(PosSymbol qualifier, PosSymbol name,
+            Class<T> entryType) {
+        this(qualifier, name, entryType, ImportStrategy.IMPORT_NONE,
                 FacilityStrategy.FACILITY_IGNORE, false);
     }
 
