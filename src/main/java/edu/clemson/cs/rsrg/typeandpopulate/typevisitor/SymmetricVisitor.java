14d1d479074bf40cac0852e97248929f87ac839d
Yu-Shan Sun <yushans@g.clemson.edu>
2016-07-27 17:34:51 -0400

    Begin refactoring SymmetricVisitor

---
+++ src/main/java/edu/clemson/cs/rsrg/typeandpopulate/typevisitor/SymmetricVisitor.java
@@ -0,0 +1,14 @@
+package edu.clemson.cs.rsrg.typeandpopulate.typevisitor;
+
+/**
+ * <p></p>
+ *
+ * <p>Note that instances of SymmetricVisitor are not thread-safe.</p>
+ *
+ * @version 2.0
+ */
+abstract class SymmetricVisitor {
+
+    
+
+}
