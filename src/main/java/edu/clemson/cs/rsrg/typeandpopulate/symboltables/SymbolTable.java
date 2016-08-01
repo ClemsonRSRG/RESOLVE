6eb5acd04da5c9a1c1ee5c3ed289bfe3071fe5f5
Yu-Shan Sun <yushans@g.clemson.edu>
2016-07-31 20:01:37 -0400

    Added JavaDocs for SymbolTable

--- src/main/java/edu/clemson/cs/rsrg/typeandpopulate/symboltables/SymbolTable.java
+++ src/main/java/edu/clemson/cs/rsrg/typeandpopulate/symboltables/SymbolTable.java
@@ -18,21 +18,70 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
- * TODO: Refactor this class
+ * <p>A <code>SymbolTable</code> stores all the {@link SymbolTableEntry}
+ * introduced by a RESOLVE module and created by the compiler.</p>
+ *
+ * @version 2.0
  */
 public interface SymbolTable extends Iterable<SymbolTableEntry> {
 
-    void put(String name, SymbolTableEntry entry);
-
-    void putAll(Map<String, SymbolTableEntry> source);
-
-    SymbolTableEntry get(String name);
-
+    /**
+     * <p>This method checks to see if there is an entry
+     * with the given <code>name</code>.</p>
+     *
+     * @param name A string name.
+     *
+     * @return {@code true} if there is an entry with this name,
+     * {@code false} otherwise.
+     */
     boolean containsKey(String name);
 
+    /**
+     * <p>This method returns the entry specified by the
+     * name.</p>
+     *
+     * @param name Name of an entry.
+     *
+     * @return A {@link SymbolTableEntry} object.
+     */
+    SymbolTableEntry get(String name);
+
+    /**
+     * <p>This method creates an iterator for the type <code>T</code>.</p>
+     *
+     * @param type A type name.
+     * @param <T> A {@link SymbolTableEntry} type.
+     *
+     * @return An {@link Iterator}.
+     */
     <T extends SymbolTableEntry> Iterator<T> iterateByType(Class<T> type);
 
+    /**
+     * <p>This method creates an iterator for to iterate over
+     * a collection of <code>T</code>.</p>
+     *
+     * @param types A type name.
+     * @param <T> A {@link SymbolTableEntry} type.
+     *
+     * @return An {@link Iterator}.
+     */
     <T extends SymbolTableEntry> Iterator<T> iterateByType(
             Collection<Class<T>> types);
+
+    /**
+     * <p>This method puts an entry into the symbol table.</p>
+     *
+     * @param name Name of an entry.
+     * @param entry The entry to be put into the table.
+     */
+    void put(String name, SymbolTableEntry entry);
+
+    /**
+     * <p>This method puts all the entries from the source
+     * into the symbol table.</p>
+     *
+     * @param source A source map of entries.
+     */
+    void putAll(Map<String, SymbolTableEntry> source);
 
 }
