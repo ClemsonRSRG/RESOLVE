72cc317d67a91b0b211290cf03d73e9ae1a50875
Yu-Shan Sun <yushans@g.clemson.edu>
2016-08-13 18:51:53 -0400

    Begin refactoring MathSymbolTableBuilder

--- src/main/java/edu/clemson/cs/rsrg/typeandpopulate/symboltables/MathSymbolTableBuilder.java
+++ src/main/java/edu/clemson/cs/rsrg/typeandpopulate/symboltables/MathSymbolTableBuilder.java
@@ -14,16 +14,58 @@ package edu.clemson.cs.rsrg.typeandpopulate.symboltables;
 
 import edu.clemson.cs.rsrg.absyn.ResolveConceptualElement;
 import edu.clemson.cs.rsrg.absyn.declarations.moduledecl.ModuleDec;
+import edu.clemson.cs.rsrg.typeandpopulate.exception.NoSuchScopeException;
 import edu.clemson.cs.rsrg.typeandpopulate.exception.NoSuchSymbolException;
 import edu.clemson.cs.rsrg.typeandpopulate.typereasoning.TypeGraph;
 import edu.clemson.cs.rsrg.typeandpopulate.utilities.ModuleIdentifier;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 /**
- * TODO: Refactor this class
+ * <p>A <code>MathSymbolTableBuilder</code> is a factory for producing immutable
+ * {@link MathSymbolTable MathSymbolTables}. It's behavior directly mirrors
+ * <code>MathSymbolTable</code>, so that it can be used as a working symbol
+ * table while it is built.</p>
+ *
+ * <p>Once the building process is complete, {@link #seal()} should
+ * be called to return a <code>MathSymbolTable</code> that is equivalent to
+ * the working symbol table represented by this
+ * <code>MathSymbolTableBuilder</code>.</p>
+ *
+ * @version 2.0
  */
 public class MathSymbolTableBuilder extends ScopeRepository {
 
-    public ScopeBuilder endScope() {
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    private static final Scope DUMMY_RESOLVER = new DummyIdentifierResolver();
+
+    private final Deque<ScopeBuilder> myLexicalScopeStack =
+            new LinkedList<>();
+
+    private final Map<ResolveConceptualElement, ScopeBuilder> myScopes =
+            new HashMap<>();
+
+    private final Map<ModuleIdentifier, ModuleScopeBuilder> myModuleScopes =
+            new HashMap<>();
+
+    private ModuleScopeBuilder myCurModuleScope = null;
+
+    private final TypeGraph myTypeGraph;
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    public final ScopeBuilder endScope() {
         return null;
     }
 
@@ -32,12 +74,14 @@ public class MathSymbolTableBuilder extends ScopeRepository {
      * {@link ModuleIdentifier}.</p>
      *
      * @param module The module identifier.
-     * @throws NoSuchSymbolException If no scope has been opened for the named
-     *                               module.
-     * @returns The associated module scope.
+     *
+     * @return The associated module scope.
+     *
+     * @throws NoSuchSymbolException If no scope has been opened for
+     * the named module.
      */
     @Override
-    public ModuleScope getModuleScope(ModuleIdentifier module)
+    public final ModuleScope getModuleScope(ModuleIdentifier module)
             throws NoSuchSymbolException {
         return null;
     }
@@ -47,20 +91,30 @@ public class MathSymbolTableBuilder extends ScopeRepository {
      * defining element.</p>
      *
      * @param e defining element.
-     * @throws NoSuchSymbolException If no scope has been opened for the given
-     *                               defining element.
-     * @returns The associated scope.
+     *
+     * @return The associated scope.
+     *
+     * @throws NoSuchScopeException If no scope has been opened for
+     * the given defining element.
      */
     @Override
-    public Scope getScope(ResolveConceptualElement e) {
+    public final Scope getScope(ResolveConceptualElement e)
+            throws NoSuchScopeException {
         return null;
     }
 
-    public TypeGraph getTypeGraph() {
+    /**
+     * <p>Returns the {@link TypeGraph} that relates the types found in this
+     * <code>MathSymbolTable</code>.</p>
+     *
+     * @return The {@link TypeGraph} object.
+     */
+    @Override
+    public final TypeGraph getTypeGraph() {
         return null;
     }
 
-    public ModuleScopeBuilder startModuleScope(ModuleDec definingElement) {
+    public final ModuleScopeBuilder startModuleScope(ModuleDec definingElement) {
         return null;
     }
 
