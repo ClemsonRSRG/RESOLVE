f70a46dcb6fccc57daef366da4b198f549ed981e
Yu-Shan Sun <yushans@g.clemson.edu>
2017-02-16 11:14:59 -0500

    Begin adding a grammar test suite

---
+++ test/java/edu/clemson/cs/rsrg/parsing/GrammarTestSuite.java
@@ -0,0 +1,29 @@
+/**
+ * GrammarTestSuite.java
+ * ---------------------------------
+ * Copyright (c) 2016
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.parsing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {
+
+})
+/**
+ * <p>A JUnit test suite for checking the RESOLVE compiler's
+ * Lexer and Parser.</p>
+ *
+ * @author Yu-Shan Sun
+ * @version 1.0
+ */
+public class GrammarTestSuite {}
