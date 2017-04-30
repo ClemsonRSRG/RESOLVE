920b433fe1d4a5789301c54b4788c430555c8c34
Yu-Shan Sun <yushans@g.clemson.edu>
2017-04-30 15:31:07 -0400

    Sanity check preserves mode

--- src/main/java/edu/clemson/cs/rsrg/typeandpopulate/sanitychecking/ValidFunctionCallChecker.java
+++ src/main/java/edu/clemson/cs/rsrg/typeandpopulate/sanitychecking/ValidFunctionCallChecker.java
@@ -12,7 +12,6 @@
  */
 package edu.clemson.cs.rsrg.typeandpopulate.sanitychecking;
 
-import edu.clemson.cs.rsrg.absyn.declarations.operationdecl.OperationProcedureDec;
 import edu.clemson.cs.rsrg.absyn.expressions.Exp;
 import edu.clemson.cs.rsrg.absyn.expressions.programexpr.*;
 import edu.clemson.cs.rsrg.parsing.data.Location;
@@ -45,16 +44,7 @@ public class ValidFunctionCallChecker {
     /** <p>The location that generated this checker.</p> */
     private final Location myLocation;
 
-    /**
-     * <p>While we walk the children of an operation, {@link OperationProcedureDec}, or
-     * procedure, this list will contain all formal parameters encountered so
-     * far, otherwise it will be null.  Since none of these structures can be
-     * be nested, there's no need for a stack.</p>
-     *
-     * <p>If you need to distinguish if you're in the middle of an
-     * operation/{@link OperationProcedureDec} or a procedure, check
-     * {@code myCorrespondingOperation}.</p>
-     */
+    /** <p>The current procedure's parameter entries.</p> */
     private final List<ProgramParameterEntry> myProcedureParameters;
 
     // ===========================================================
@@ -109,8 +99,62 @@ public class ValidFunctionCallChecker {
                         || argExp instanceof ProgramFunctionExp
                         || argExp instanceof ProgramIntegerExp || argExp instanceof ProgramStringExp)) {
                     throw new SourceErrorException(
-                            "An EVALUATES mode can only accept program character/function/integer/string expressions. "
-                                    + "Found: " + argExp, myLocation);
+                            "An EVALUATES mode can only accept program"
+                                    + " character/function/integer/string expressions.\n"
+                                    + "Found a program expression: " + argExp +
+                                    " with type: " + argExp.getProgramType(), myLocation);
+                }
+            }
+        }
+
+        // YS: Loop through our current procedure's parameters
+        // If we see a preserves mode parameter, make sure it is only
+        // being passed to an operation that also preserves that parameter.
+        for (ProgramParameterEntry procParam : myProcedureParameters) {
+            if (procParam.getParameterMode().equals(ParameterMode.PRESERVES)) {
+                paramIt = myCorrespondingOperation.getParameters().iterator();
+                argIt = myCallingFunctionExp.getArguments().iterator();
+
+                while (argIt.hasNext()) {
+                    ProgramParameterEntry param = paramIt.next();
+                    ProgramExp argExp = argIt.next();
+
+                    if (argExp instanceof ProgramVariableExp) {
+                        if (argExp instanceof ProgramVariableNameExp) {
+                            ProgramVariableNameExp argExpAsProgVarNameExp =
+                                    (ProgramVariableNameExp) argExp;
+                            if (argExpAsProgVarNameExp.getName().getName()
+                                    .equals(procParam.getName())
+                                    && !param.getParameterMode().equals(
+                                            ParameterMode.PRESERVES)) {
+                                throw new SourceErrorException(
+                                        "Expression: "
+                                                + argExp
+                                                + " has PRESERVES mode and cannot be passed to an operation with "
+                                                + param.getParameterMode()
+                                                + " mode.", myLocation);
+                            }
+                        }
+                        else if (argExp instanceof ProgramVariableDotExp) {
+                            ProgramVariableDotExp argExpAsProgVarDotExp =
+                                    (ProgramVariableDotExp) argExp;
+                            ProgramVariableExp firstExp =
+                                    argExpAsProgVarDotExp.getSegments().get(0);
+                            if (firstExp instanceof ProgramVariableNameExp
+                                    && ((ProgramVariableNameExp) firstExp)
+                                            .getName().getName().equals(
+                                                    procParam.getName())
+                                    && !param.getParameterMode().equals(
+                                            ParameterMode.PRESERVES)) {
+                                throw new SourceErrorException(
+                                        "Expression: "
+                                                + argExp
+                                                + " has PRESERVES mode and cannot be passed to an operation with "
+                                                + param.getParameterMode()
+                                                + " mode.", myLocation);
+                            }
+                        }
+                    }
                 }
             }
         }
