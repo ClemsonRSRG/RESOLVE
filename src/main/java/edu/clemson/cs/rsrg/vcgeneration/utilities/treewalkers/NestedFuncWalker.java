8c37f744439163e1c25ac6444a27a02b305e8e9b
Yu-Shan Sun <yushans@g.clemson.edu>
2017-06-04 20:12:16 -0400

    Used the logic from callstmt as base

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/utilities/treewalkers/NestedFuncWalker.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/utilities/treewalkers/NestedFuncWalker.java
@@ -15,9 +15,12 @@ package edu.clemson.cs.rsrg.vcgeneration.utilities.treewalkers;
 import edu.clemson.cs.rsrg.absyn.clauses.AssertionClause;
 import edu.clemson.cs.rsrg.absyn.declarations.variabledecl.ParameterVarDec;
 import edu.clemson.cs.rsrg.absyn.expressions.Exp;
+import edu.clemson.cs.rsrg.absyn.expressions.mathexpr.VarExp;
 import edu.clemson.cs.rsrg.absyn.expressions.programexpr.ProgramExp;
 import edu.clemson.cs.rsrg.absyn.expressions.programexpr.ProgramFunctionExp;
+import edu.clemson.cs.rsrg.parsing.data.PosSymbol;
 import edu.clemson.cs.rsrg.treewalk.TreeWalkerVisitor;
+import edu.clemson.cs.rsrg.vcgeneration.utilities.Utilities;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -114,7 +117,42 @@ public class NestedFuncWalker extends TreeWalkerVisitor {
      */
     private Exp replaceFormalWithActualReq(Exp requires,
             List<ParameterVarDec> paramList, List<ProgramExp> argList) {
-        return null;
+        // YS: We need two replacement maps in case we happen to have the
+        // same names in formal parameter arguments and in the argument list.
+        Map<Exp, Exp> paramToTemp = new HashMap<>();
+        Map<Exp, Exp> tempToActual = new HashMap<>();
+
+        // Replace precondition variables in the requires clause
+        for (int i = 0; i < argList.size(); i++) {
+            ParameterVarDec varDec = paramList.get(i);
+            Exp exp = argList.get(i);
+
+            // Convert the pExp into a something we can use
+            Exp repl = Utilities.convertExp(exp, myCurrentModuleScope);
+
+            // VarExp form of the parameter variable
+            VarExp oldExp =
+                    Utilities.createVarExp(varDec.getLocation(), null,
+                            varDec.getName(), exp.getMathType(), exp.getMathTypeValue());
+
+            // New VarExp
+            VarExp newExp =
+                    Utilities.createVarExp(varDec.getLocation(), null,
+                            new PosSymbol(varDec.getLocation(), "_" + varDec.getName().getName()),
+                            repl.getMathType(), repl.getMathTypeValue());
+
+            // Add a substitution entry from formal parameter to temp
+            paramToTemp.put(oldExp, newExp);
+
+            // Add a substitution entry from temp to actual parameter
+            tempToActual.put(newExp, repl);
+        }
+
+        // Replace from formal to temp and then from temp to actual
+        requires = requires.substitute(paramToTemp);
+        requires = requires.substitute(tempToActual);
+
+        return requires;
     }
 
 }
