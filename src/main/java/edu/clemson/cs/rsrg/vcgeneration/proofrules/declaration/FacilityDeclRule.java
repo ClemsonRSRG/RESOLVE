d3cb0138ac160beb6a3053db982d6ba5daabba28
Yu-Shan Sun <yushans@g.clemson.edu>
2017-06-30 11:46:55 -0400

    An helper method to convert module arguments to exps

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/declaration/FacilityDeclRule.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/declaration/FacilityDeclRule.java
@@ -16,10 +16,15 @@ import edu.clemson.cs.rsrg.absyn.declarations.Dec;
 import edu.clemson.cs.rsrg.absyn.declarations.facilitydecl.FacilityDec;
 import edu.clemson.cs.rsrg.absyn.declarations.moduledecl.ConceptModuleDec;
 import edu.clemson.cs.rsrg.absyn.declarations.paramdecl.ModuleParameterDec;
+import edu.clemson.cs.rsrg.absyn.expressions.Exp;
 import edu.clemson.cs.rsrg.absyn.expressions.mathexpr.VarExp;
+import edu.clemson.cs.rsrg.absyn.expressions.programexpr.ProgramExp;
+import edu.clemson.cs.rsrg.absyn.expressions.programexpr.ProgramFunctionExp;
+import edu.clemson.cs.rsrg.absyn.items.programitems.ModuleArgumentItem;
 import edu.clemson.cs.rsrg.statushandling.exception.MiscErrorException;
 import edu.clemson.cs.rsrg.typeandpopulate.exception.NoSuchSymbolException;
 import edu.clemson.cs.rsrg.typeandpopulate.symboltables.MathSymbolTableBuilder;
+import edu.clemson.cs.rsrg.typeandpopulate.symboltables.ModuleScope;
 import edu.clemson.cs.rsrg.typeandpopulate.utilities.ModuleIdentifier;
 import edu.clemson.cs.rsrg.vcgeneration.proofrules.AbstractProofRuleApplication;
 import edu.clemson.cs.rsrg.vcgeneration.proofrules.ProofRuleApplication;
@@ -44,6 +49,12 @@ public class FacilityDeclRule extends AbstractProofRuleApplication
     // Member Fields
     // ===========================================================
 
+    /**
+     * <p>The module scope for the file we are generating
+     * {@code VCs} for.</p>
+     */
+    private final ModuleScope myCurrentModuleScope;
+
     /** <p>The {@code facility} declaration we are applying the rule to.</p> */
     private final FacilityDec myFacilityDec;
 
@@ -65,15 +76,17 @@ public class FacilityDeclRule extends AbstractProofRuleApplication
      *                    rule to.
      * @param isLocalFacDec A flag that indicates if this is a local {@link FacilityDec}.
      * @param symbolTableBuilder The current symbol table.
+     * @param moduleScope The current module scope we are visiting.
      * @param block The assertive code block that the subclasses are
      *              applying the rule to.
      * @param stGroup The string template group we will be using.
      * @param blockModel The model associated with {@code block}.
      */
     public FacilityDeclRule(FacilityDec facilityDec, boolean isLocalFacDec,
-            MathSymbolTableBuilder symbolTableBuilder,
+            MathSymbolTableBuilder symbolTableBuilder, ModuleScope moduleScope,
             AssertiveCodeBlock block, STGroup stGroup, ST blockModel) {
         super(block, stGroup, blockModel);
+        myCurrentModuleScope = moduleScope;
         myFacilityDec = facilityDec;
         myIsLocalFacilityDec = isLocalFacDec;
         mySymbolTable = symbolTableBuilder;
@@ -128,6 +141,43 @@ public class FacilityDeclRule extends AbstractProofRuleApplication
     // ===========================================================
 
     /**
+     * <p>An helper method that creates a list of {@link Exp Exps}
+     * representing each of the {@link ModuleArgumentItem ModuleArgumentItems}.
+     * It is possible that the passed in programming expression
+     * contains nested function calls, therefore we will need to deal
+     * with it appropriately.</p>
+     *
+     * @param actualParams List of module instantiated arguments.
+     *
+     * @return A list containing the {@link Exp Exps} representing
+     * each actual argument.
+     */
+    private List<Exp> createModuleArgExpList(List<ModuleArgumentItem> actualParams) {
+        List<Exp> retExpList = new ArrayList<>();
+
+        for (ModuleArgumentItem item : actualParams) {
+            // Convert the module argument items into the equivalent
+            // mathematical expression.
+            ProgramExp moduleArgumentExp = item.getArgumentExp();
+            Exp moduleArgumentAsExp;
+            if (moduleArgumentExp instanceof ProgramFunctionExp) {
+                // TODO: Use the program function expression walker to generate moduleArgumentAsExp.
+                throw new MiscErrorException("[VCGenerator] Insert program function expression walker here!",
+                        new RuntimeException());
+            }
+            else {
+                // Simply convert to the math equivalent expression
+                moduleArgumentAsExp = Utilities.convertExp(moduleArgumentExp, myCurrentModuleScope);
+            }
+
+            // Add this to our return list
+            retExpList.add(moduleArgumentAsExp);
+        }
+
+        return retExpList;
+    }
+
+    /**
      * <p>An helper method that creates a list of {@link VarExp VarExps}
      * representing each of the {@link ModuleParameterDec ModuleParameterDecs}.</p>
      *
@@ -135,9 +185,6 @@ public class FacilityDeclRule extends AbstractProofRuleApplication
      *
      * @return A list containing the {@link VarExp VarExps} representing
      * each formal parameter.
-     *
-     * @throws MiscErrorException An error is thrown if we don't know how to handle
-     * a particular kind of {@link ModuleParameterDec}.
      */
     private List<VarExp> createModuleParamExpList(List<ModuleParameterDec> formalParams) {
         List<VarExp> retExpList = new ArrayList<>(formalParams.size());
