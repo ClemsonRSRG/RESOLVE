ed7d1197d04fbed80510c30d95f13827aba5b4c5
Yu-Shan Sun <yushans@g.clemson.edu>
2017-09-09 13:54:30 -0400

    Begin refactoring translator

---
+++ src/main/java/edu/clemson/cs/rsrg/translation/AbstractTranslator.java
@@ -0,0 +1,57 @@
+/*
+ * AbstractTranslator.java
+ * ---------------------------------
+ * Copyright (c) 2017
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.translation;
+
+import edu.clemson.cs.rsrg.treewalk.TreeWalkerStackVisitor;
+
+/**
+ * <p>This class generates verification conditions (VCs) using the provided
+ * RESOLVE abstract syntax tree. This visitor logic is implemented as
+ * a {@link TreeWalkerStackVisitor}.</p>
+ *
+ * @author Daniel Welch
+ * @author Mark Todd
+ * @author Yu-Shan Sun
+ * @version 2.0
+ */
+public abstract class AbstractTranslator extends TreeWalkerStackVisitor {
+
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    // ===========================================================
+    // Flag Strings
+    // ===========================================================
+
+    // ===========================================================
+    // Flags
+    // ===========================================================
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Visitor Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Protected Methods
+    // ===========================================================
+
+}
