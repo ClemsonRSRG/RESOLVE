5d2ad3a3bef9fab5a31247e0c194d85972c6fbc7
Yu-Shan Sun <yushans@g.clemson.edu>
2017-09-12 11:25:48 -0400

    The two supported translation targets

---
+++ src/main/java/edu/clemson/cs/rsrg/translation/targets/CTranslator.java
@@ -0,0 +1,60 @@
+/*
+ * CTranslator.java
+ * ---------------------------------
+ * Copyright (c) 2017
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.translation.targets;
+
+import edu.clemson.cs.rsrg.translation.AbstractTranslator;
+
+/**
+ * <p>This class translates a {@code RESOLVE} source file
+ * to {@code C}.</p>
+ *
+ * @author Daniel Welch
+ * @author Mark Todd
+ * @author Yu-Shan Sun
+ * @version 2.0
+ */
+public class CTranslator extends AbstractTranslator {
+
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    // ===========================================================
+    // Flag Strings
+    // ===========================================================
+
+    // ===========================================================
+    // Flags
+    // ===========================================================
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Visitor Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Protected Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Private Methods
+    // ===========================================================
+
+}
---
+++ src/main/java/edu/clemson/cs/rsrg/translation/targets/JavaTranslator.java
@@ -0,0 +1,59 @@
+/*
+ * JavaTranslator.java
+ * ---------------------------------
+ * Copyright (c) 2017
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.translation.targets;
+
+import edu.clemson.cs.rsrg.translation.AbstractTranslator;
+
+/**
+ * <p>This class translates a {@code RESOLVE} source file
+ * to {@code Java}.</p>
+ *
+ * @author Daniel Welch
+ * @author Yu-Shan Sun
+ * @version 2.0
+ */
+public class JavaTranslator extends AbstractTranslator {
+
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    // ===========================================================
+    // Flag Strings
+    // ===========================================================
+
+    // ===========================================================
+    // Flags
+    // ===========================================================
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Visitor Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Protected Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Private Methods
+    // ===========================================================
+
+}
