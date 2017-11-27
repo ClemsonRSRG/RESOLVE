a46f1b00e9b5cbc4b1a03c421390ae2522566530
Yu-Shan Sun <yushans@g.clemson.edu>
2017-11-27 13:16:02 -0500

    If-Else Rule

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/statement/IfStmtRule.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/statement/IfStmtRule.java
@@ -12,12 +12,29 @@
  */
 package edu.clemson.cs.rsrg.vcgeneration.proofrules.statement;
 
+import edu.clemson.cs.rsrg.absyn.declarations.typedecl.AbstractTypeRepresentationDec;
+import edu.clemson.cs.rsrg.absyn.declarations.typedecl.TypeFamilyDec;
+import edu.clemson.cs.rsrg.absyn.expressions.Exp;
+import edu.clemson.cs.rsrg.absyn.expressions.mathexpr.MathExp;
+import edu.clemson.cs.rsrg.absyn.expressions.mathexpr.VarExp;
 import edu.clemson.cs.rsrg.absyn.expressions.programexpr.ProgramExp;
+import edu.clemson.cs.rsrg.absyn.expressions.programexpr.ProgramFunctionExp;
 import edu.clemson.cs.rsrg.absyn.items.programitems.IfConditionItem;
+import edu.clemson.cs.rsrg.absyn.statements.AssumeStmt;
+import edu.clemson.cs.rsrg.absyn.statements.ConfirmStmt;
 import edu.clemson.cs.rsrg.absyn.statements.IfStmt;
+import edu.clemson.cs.rsrg.treewalk.TreeWalker;
+import edu.clemson.cs.rsrg.typeandpopulate.symboltables.MathSymbolTableBuilder;
+import edu.clemson.cs.rsrg.typeandpopulate.symboltables.ModuleScope;
+import edu.clemson.cs.rsrg.typeandpopulate.typereasoning.TypeGraph;
 import edu.clemson.cs.rsrg.vcgeneration.proofrules.AbstractProofRuleApplication;
 import edu.clemson.cs.rsrg.vcgeneration.proofrules.ProofRuleApplication;
 import edu.clemson.cs.rsrg.vcgeneration.utilities.AssertiveCodeBlock;
+import edu.clemson.cs.rsrg.vcgeneration.utilities.LocationDetailModel;
+import edu.clemson.cs.rsrg.vcgeneration.utilities.Utilities;
+import edu.clemson.cs.rsrg.vcgeneration.utilities.formaltoactual.InstantiatedFacilityDecl;
+import edu.clemson.cs.rsrg.vcgeneration.utilities.treewalkers.ProgramFunctionExpWalker;
+import java.util.List;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 
@@ -36,8 +53,40 @@ public class IfStmtRule extends AbstractProofRuleApplication
     // Member Fields
     // ===========================================================
 
+    /**
+     * <p>This contains all the types declared by the {@code Concept}
+     * associated with the current module. Note that if we are in a
+     * {@code Facility}, this list will be empty.</p>
+     */
+    private final List<TypeFamilyDec> myCurrentConceptDeclaredTypes;
+
+    /**
+     * <p>The module scope for the file we are generating
+     * {@code VCs} for.</p>
+     */
+    private final ModuleScope myCurrentModuleScope;
+
     /** <p>The {@link IfStmt} we are applying the rule to.</p> */
     private final IfStmt myIfStmt;
+
+    /**
+     * <p>If our current module scope allows us to introduce new type implementations,
+     * this will contain all the {@link AbstractTypeRepresentationDec}. Otherwise,
+     * this list will be empty.</p>
+     */
+    private final List<AbstractTypeRepresentationDec> myLocalRepresentationTypeDecs;
+
+    /** <p>The list of processed {@link InstantiatedFacilityDecl}. </p> */
+    private final List<InstantiatedFacilityDecl> myProcessedInstFacilityDecls;
+
+    /** <p>The symbol table we are currently building.</p> */
+    private final MathSymbolTableBuilder mySymbolTable;
+
+    /**
+     * <p>This is the math type graph that indicates relationship
+     * between different math types.</p>
+     */
+    private final TypeGraph myTypeGraph;
 
     // ===========================================================
     // Constructors
@@ -49,15 +98,29 @@ public class IfStmtRule extends AbstractProofRuleApplication
      *
      * @param ifStmt The {@link IfStmt} we are applying
      *               the rule to.
+     * @param typeFamilyDecs List of abstract types we are implementing or extending.
+     * @param localRepresentationTypeDecs List of local representation types.
+     * @param processedInstFacDecs The list of processed {@link InstantiatedFacilityDecl}.
+     * @param symbolTableBuilder The current symbol table.
+     * @param moduleScope The current module scope we are visiting.
      * @param block The assertive code block that the subclasses are
      *              applying the rule to.
      * @param stGroup The string template group we will be using.
      * @param blockModel The model associated with {@code block}.
      */
-    public IfStmtRule(IfStmt ifStmt, AssertiveCodeBlock block, STGroup stGroup,
-            ST blockModel) {
+    public IfStmtRule(IfStmt ifStmt, List<TypeFamilyDec> typeFamilyDecs,
+            List<AbstractTypeRepresentationDec> localRepresentationTypeDecs,
+            List<InstantiatedFacilityDecl> processedInstFacDecs,
+            MathSymbolTableBuilder symbolTableBuilder, ModuleScope moduleScope,
+            AssertiveCodeBlock block, STGroup stGroup, ST blockModel) {
         super(block, stGroup, blockModel);
+        myCurrentConceptDeclaredTypes = typeFamilyDecs;
+        myCurrentModuleScope = moduleScope;
         myIfStmt = ifStmt;
+        myLocalRepresentationTypeDecs = localRepresentationTypeDecs;
+        myProcessedInstFacilityDecls = processedInstFacDecs;
+        mySymbolTable = symbolTableBuilder;
+        myTypeGraph = symbolTableBuilder.getTypeGraph();
     }
 
     // ===========================================================
@@ -69,28 +132,154 @@ public class IfStmtRule extends AbstractProofRuleApplication
      */
     @Override
     public final void applyRule() {
-        // Note: In the If-Else Rule, we will end up with a total of two assertive
-        // code blocks that are almost identical. The first block will be the current
-        // assertive block that we are currently processing and will contain the
-        // logic for the if portion of the statement. Regardless if there is an else
-        // portion or not, the other block will contain the else portion.
-        AssertiveCodeBlock negIfAssertiveCodeBlock =
-                myCurrentAssertiveCodeBlock.clone();
-
-        // TODO: Convert the if-condition into mathematical expressions.
-        // TODO: Use the nested function call walker to deal with nested function calls.
+        // Convert the if-condition into mathematical expressions.
         IfConditionItem ifConditionItem = myIfStmt.getIfClause();
         ProgramExp ifCondition = ifConditionItem.getTest();
+        if (ifCondition instanceof ProgramFunctionExp) {
+            // Note: In the If-Else Rule, we will end up with a total of two assertive
+            // code blocks that are almost identical. The first block will be the current
+            // assertive block that we are currently processing and will contain the
+            // logic for the if portion of the statement. Regardless if there is an else
+            // portion or not, the other block will contain the else portion.
+            AssertiveCodeBlock negIfAssertiveCodeBlock =
+                    myCurrentAssertiveCodeBlock.clone();
 
-        // If part of the rule
+            // Use the walker to convert to mathematical expression
+            // TODO: Use the other constructor!
+            ProgramFunctionExp ifConditionAsProgramFunctionExp =
+                    (ProgramFunctionExp) ifCondition;
+            ProgramFunctionExpWalker walker =
+                    new ProgramFunctionExpWalker(myCurrentAssertiveCodeBlock,
+                            myCurrentConceptDeclaredTypes,
+                            myLocalRepresentationTypeDecs,
+                            myProcessedInstFacilityDecls, myCurrentModuleScope,
+                            myTypeGraph);
+            TreeWalker.visit(walker, ifConditionAsProgramFunctionExp);
 
-        // Else part of the rule
+            // Retrieve the various pieces of information from the walker
+            Exp generatedRequires =
+                    walker.getRequiresClause(ifConditionItem.getTest()
+                            .getLocation());
+            Exp generatedEnsures =
+                    walker.getEnsuresClause(ifConditionAsProgramFunctionExp);
+            List<Exp> restoresParamExps =
+                    walker.getRestoresParamEnsuresClauses();
+            List<ConfirmStmt> terminationConfirms =
+                    walker.getTerminationConfirmStmts();
+            myLocationDetails.putAll(walker.getNewLocationString());
 
-        // Add the different details to the various different output models
-        ST stepModel = mySTGroup.getInstanceOf("outputVCGenStep");
-        stepModel.add("proofRuleName", getRuleDescription()).add(
-                "currentStateOfBlock", myCurrentAssertiveCodeBlock);
-        myBlockModel.add("vcGenSteps", stepModel.render());
+            // Form a conjunct using the restoresParamExps
+            // YS: We also make a copy of this for the else part,
+            //     while storing the associated location details.
+            Exp restoresParamEnsuresIfPart =
+                    VarExp.getTrueVarExp(ifConditionItem.getTest()
+                            .getLocation(), myTypeGraph);
+            Exp restoresParamEnsuresElsePart =
+                    VarExp.getTrueVarExp(ifConditionItem.getTest()
+                            .getLocation(), myTypeGraph);
+            for (Exp exp : restoresParamExps) {
+                // Make a copy of the expression for the else part
+                // and add the new location detail.
+                Exp expCopy = exp.clone();
+                LocationDetailModel detailModel =
+                        myLocationDetails.get(exp.getLocation());
+                myLocationDetails
+                        .put(expCopy.getLocation(), new LocationDetailModel(
+                                detailModel.getSourceLoc(), exp.getLocation(),
+                                detailModel.getDetailMessage()));
+
+                if (VarExp.isLiteralTrue(restoresParamEnsuresIfPart)) {
+                    restoresParamEnsuresIfPart = exp;
+                    restoresParamEnsuresElsePart = expCopy;
+                }
+                else {
+                    restoresParamEnsuresIfPart =
+                            MathExp.formConjunct(ifConditionItem.getTest()
+                                    .getLocation().clone(),
+                                    restoresParamEnsuresIfPart, exp);
+                    restoresParamEnsuresElsePart =
+                            MathExp.formConjunct(ifConditionItem.getTest()
+                                    .getLocation().clone(),
+                                    restoresParamEnsuresElsePart, expCopy);
+                }
+            }
+
+            // If part of the rule
+            // 1) If the testing condition contains recursive calls,
+            //    we need to add all the termination confirm statements
+            //    to the if assertive code block.
+            for (ConfirmStmt confirmStmt : terminationConfirms) {
+                myCurrentAssertiveCodeBlock.addStatement(confirmStmt);
+            }
+
+            // 2) If the testing condition has any requires clauses,
+            //    we need to add it as a new confirm statement.
+            //    ( Confirm Invk_Cond(BE) )
+            myCurrentAssertiveCodeBlock.addStatement(new ConfirmStmt(
+                    ifConditionItem.getTest().getLocation().clone(),
+                    generatedRequires, false));
+
+            // 3) Add the testing condition as a new stipulate assume statement.
+            //    ( Stipulate Math(BE) )
+            Exp ifConditionBEExp = generatedEnsures.clone();
+            myLocationDetails.put(ifConditionBEExp.getLocation(),
+                    new LocationDetailModel(generatedEnsures.getLocation(),
+                            ifConditionBEExp.getLocation(),
+                            "If Statement Condition"));
+            myCurrentAssertiveCodeBlock.addStatement(new AssumeStmt(
+                    ifConditionItem.getTest().getLocation().clone(),
+                    ifConditionBEExp, true));
+
+            // 4) If the ProgramFunctionExp walker generated any restores
+            //    parameter ensures clauses, we need to add it as a new
+            //    assume statement.
+            if (!VarExp.isLiteralTrue(restoresParamEnsuresIfPart)) {
+                myCurrentAssertiveCodeBlock.addStatement(new AssumeStmt(
+                        ifConditionItem.getTest().getLocation().clone(),
+                        restoresParamEnsuresIfPart, false));
+            }
+
+            // 5) Add all the statements inside the if-part to the
+            //    if-assertive code block.
+            myCurrentAssertiveCodeBlock.addStatements(ifConditionItem
+                    .getStatements());
+
+            // NY YS
+            // TODO: Duration for If Part
+
+            // TODO: 6) Update the location detail for the VCs
+
+            // 7) Add the different details to the various different output models
+            ST stepModel = mySTGroup.getInstanceOf("outputVCGenStep");
+            stepModel.add("proofRuleName", "If-Part Rule").add(
+                    "currentStateOfBlock", myCurrentAssertiveCodeBlock);
+            myBlockModel.add("vcGenSteps", stepModel.render());
+
+            // Else part of the rule
+            // 1) Add the testing condition as a new stipulate assume statement.
+            //    ( Stipulate not(Math(BE)) )
+            Exp elseConditionBEExp = generatedEnsures.clone();
+            elseConditionBEExp = Utilities.negateExp(elseConditionBEExp, myTypeGraph.BOOLEAN);
+            myLocationDetails.put(elseConditionBEExp.getLocation(),
+                    new LocationDetailModel(generatedEnsures.getLocation(),
+                            elseConditionBEExp.getLocation(),
+                            "Negation of If Statement Condition"));
+            negIfAssertiveCodeBlock.addStatement(new AssumeStmt(
+                    ifConditionItem.getTest().getLocation().clone(),
+                    elseConditionBEExp, true));
+
+            // 2) Add all the statements inside the else-part to the
+            //    else-assertive code block.
+            negIfAssertiveCodeBlock.addStatements(myIfStmt.getElseclause());
+
+            // NY YS
+            // TODO: Duration for Else Part
+
+            myResultingAssertiveCodeBlocks.add(negIfAssertiveCodeBlock);
+        }
+        else {
+            Utilities.expNotHandled(ifCondition, myIfStmt.getLocation());
+        }
     }
 
     /**
