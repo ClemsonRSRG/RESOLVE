e19d1a1824a75adb1bee81220bdf83be0c2fdad0
Yu-Shan Sun <yushans@g.clemson.edu>
2018-01-10 14:27:20 -0500

    License

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/other/WhichEntailsRule.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/other/WhichEntailsRule.java
@@ -1,3 +1,15 @@
+/*
+ * WhichEntailsRule.java
+ * ---------------------------------
+ * Copyright (c) 2017
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
 package edu.clemson.cs.rsrg.vcgeneration.proofrules.other;
 
 import edu.clemson.cs.rsrg.absyn.clauses.AssertionClause;
