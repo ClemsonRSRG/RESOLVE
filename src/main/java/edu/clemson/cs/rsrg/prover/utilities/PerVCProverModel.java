19cc64728f8b5f41412c65f34c2e23fcbc8405cb
Yu-Shan Sun <yushans@g.clemson.edu>
2018-05-14 10:41:52 -0400

    PerVCProverModel needs to be filled in

---
+++ src/main/java/edu/clemson/cs/rsrg/prover/utilities/PerVCProverModel.java
@@ -0,0 +1,24 @@
+/*
+ * PerVCProverModel.java
+ * ---------------------------------
+ * Copyright (c) 2017
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.prover.utilities;
+
+/**
+ * <p>This class contains all the different information on each {@code VC}
+ * that comes from the automated prover.</p>
+ *
+ * @author Hampton Smith
+ * @version 2.0
+ */
+public final class PerVCProverModel {
+
+}
