68eb0f9a506c3dd2d4febfe452e43bcaf88b5f19
Yu-Shan Sun <yushans@g.clemson.edu>
2018-07-12 10:51:37 -0400

    Minor change

--- src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/declarations/sharedstatedecl/SharedStateCorrRule.java
+++ src/main/java/edu/clemson/cs/rsrg/vcgeneration/proofrules/declarations/sharedstatedecl/SharedStateCorrRule.java
@@ -10,7 +10,7 @@
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
-package edu.clemson.cs.rsrg.vcgeneration.proofrules.other;
+package edu.clemson.cs.rsrg.vcgeneration.proofrules.declarations.sharedstatedecl;
 
 import edu.clemson.cs.rsrg.absyn.declarations.sharedstatedecl.SharedStateRealizationDec;
 import edu.clemson.cs.rsrg.vcgeneration.proofrules.AbstractProofRuleApplication;
