51944d3eb054ddd0d2511a7ce704e4bb0978f2bf
Yu-Shan Sun <yushans@g.clemson.edu>
2018-09-28 10:06:44 -0400

    Added PAlternative, PLambda and PExpVisitor skeleton files

---
+++ src/main/java/edu/clemson/cs/rsrg/prover/absyn/expressions/PAlternatives.java
@@ -0,0 +1,43 @@
+/*
+ * PAlternatives.java
+ * ---------------------------------
+ * Copyright (c) 2018
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.prover.absyn.expressions;
+
+/**
+ * <p></p>
+ *
+ * @author Daniel Welch
+ * @version 2.0
+ */
+public class PAlternatives {
+
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Protected Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Private Methods
+    // ===========================================================
+
+}
---
+++ src/main/java/edu/clemson/cs/rsrg/prover/absyn/expressions/PLambda.java
@@ -0,0 +1,44 @@
+/*
+ * PLambda.java
+ * ---------------------------------
+ * Copyright (c) 2018
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.prover.absyn.expressions;
+
+/**
+ * <p></p>
+ *
+ * @author Hampton Smith
+ * @author Mike Kabbani
+ * @version 2.0
+ */
+public class PLambda {
+
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Protected Methods
+    // ===========================================================
+
+    // ===========================================================
+    // Private Methods
+    // ===========================================================
+
+}
