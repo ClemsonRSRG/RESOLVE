308e367012fa578beb4af27983737fa069f3c5b1
Yu-Shan Sun <yushans@g.clemson.edu>
2018-10-12 09:32:28 -0400

    Iterator interface for iterating PExp sub-expressions

---
+++ src/main/java/edu/clemson/cs/rsrg/prover/absyn/iterators/PExpSubexpressionIterator.java
@@ -0,0 +1,62 @@
+/*
+ * PExpSubexpressionIterator.java
+ * ---------------------------------
+ * Copyright (c) 2018
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package edu.clemson.cs.rsrg.prover.absyn.iterators;
+
+import edu.clemson.cs.rsrg.prover.absyn.PExp;
+import java.util.NoSuchElementException;
+
+/**
+ * <p>A {@code PExpSubexpressionIterator} defines the interface for classes
+ * that iterate over the sub-expressions of a {@link PExp}, with the ability to get
+ * a version of the original {@code PExp} in which any given sub-expression
+ * has been replaced with another.</p>
+ *
+ * @author Hampton Smith
+ * @version 2.0
+ */
+public interface PExpSubexpressionIterator {
+
+    /**
+     * <p>This method returns {@code true} <strong>iff</strong> there are additional
+     * sub-expressions. I.e., returns {@code true} <strong>iff</strong>
+     * {@link #next()} would return an element rather than throwing an
+     * exception.</p>
+     * 
+     * @return {@code true} if the iterator has more elements,
+     * {@code false} otherwise.
+     */
+    boolean hasNext();
+
+    /**
+     * <p>This method returns the next sub-expression./p>
+     * 
+     * @return The next element in the iteration.
+     * 
+     * @throws NoSuchElementException If there are no further subexpressions.
+     */
+    PExp next();
+
+    /**
+     * <p>This method returns a version of the original {@link PExp} (i.e., the
+     * {@code PExp} over whose sub-expressions we are iterating) with the
+     * sub-expression most recently returned by {@link #next()} replaced with
+     * {@code newExpression}.</p>
+     * 
+     * @param newExpression The argument to replace the most recently returned 
+     *                      one with.
+     *                    
+     * @return The new version.
+     */
+    PExp replaceLast(PExp newExpression);
+
+}
