4e7ffed6725b92c7317fc606a2dbec8ad45e046f
Yu-Shan Sun <yushans@g.clemson.edu>
2018-10-12 10:53:20 -0400

    Most of the methods in PExp

--- src/main/java/edu/clemson/cs/rsrg/prover/absyn/PExp.java
+++ src/main/java/edu/clemson/cs/rsrg/prover/absyn/PExp.java
@@ -13,11 +13,15 @@
 package edu.clemson.cs.rsrg.prover.absyn;
 
 import edu.clemson.cs.rsrg.absyn.expressions.Exp;
+import edu.clemson.cs.rsrg.parsing.data.PosSymbol;
 import edu.clemson.cs.rsrg.prover.absyn.expressions.PSymbol;
+import edu.clemson.cs.rsrg.prover.absyn.iterators.PExpSubexpressionIterator;
 import edu.clemson.cs.rsrg.prover.absyn.treewalkers.PExpVisitor;
 import edu.clemson.cs.rsrg.prover.exception.BindingException;
 import edu.clemson.cs.rsrg.prover.immutableadts.ImmutableList;
 import edu.clemson.cs.rsrg.typeandpopulate.mathtypes.MTType;
+import edu.clemson.cs.rsrg.typeandpopulate.typereasoning.TypeGraph;
+import java.io.StringWriter;
 import java.util.*;
 
 /**
@@ -115,6 +119,55 @@ public abstract class PExp {
     public abstract void accept(PExpVisitor v);
 
     /**
+     * <p>This method returns a mapping of expressions that
+     * that can be bound from the target.</p>
+     *
+     * @param target The target expression.
+     *
+     * @return A mapping of binding expressions.
+     *
+     * @throws BindingException Some error occurred during binding.
+     */
+    public final Map<PExp, PExp> bindTo(PExp target) throws BindingException {
+        Map<PExp, PExp> bindings = new HashMap<>();
+
+        bindTo(target, bindings);
+
+        return bindings;
+    }
+
+    /**
+     * <p>This method binds the expressions found in our mapping to
+     * the specified target.</p>
+     *
+     * @param target The target expression.
+     * @param accumulator A mapping of expressions to be bound.
+     *
+     * @throws BindingException Some error occurred during binding.
+     */
+    public abstract void bindTo(PExp target, Map<PExp, PExp> accumulator)
+            throws BindingException;
+
+    /**
+     * <p>This method checks to see if the current expression contains
+     * an existential quantifier.</p>
+     *
+     * @return {@code true} if it does contain one, {@code false} otherwise.
+     */
+    public abstract boolean containsExistential();
+
+    /**
+     * <p>This method attempts to find an expression with the given name in our
+     * sub-expressions.</p>
+     *
+     * @param name Expression name to be searched.
+     *
+     * @return {@code true} if this expression contains a sub-expression
+     * that matches the specified name, {@code false} otherwise.
+     */
+    public abstract boolean containsName(String name);
+
+    /**
      * <p>This method must be implemented by all inherited classes
      * to override the default equals method implementation.</p>
      *
@@ -124,6 +177,29 @@ public abstract class PExp {
      */
     @Override
     public abstract boolean equals(Object o);
+
+    /**
+     * <p>This method attempts to flip all quantifiers to generate
+     * a new expression.</p>
+     *
+     * @return A new {@link PExp}.
+     */
+    public abstract PExp flipQuantifiers();
+
+    /**
+     * <p>This method returns a list of sub-expressions that are function
+     * applications in the current expression.</p>
+     *
+     * @return A list of sub-expressions that are function applications.
+     */
+    public final List<PExp> getFunctionApplications() {
+        if (myCachedFunctionApplications == null) {
+            //We're immutable, so only do this once
+            myCachedFunctionApplications = getFunctionApplicationsNoCache();
+        }
+
+        return myCachedFunctionApplications;
+    }
 
     /**
      * <p>This method gets the mathematical type associated
@@ -146,11 +222,60 @@ public abstract class PExp {
     }
 
     /**
+     * <p>This method returns a set of quantified variables for the current
+     * expression.</p>
+     *
+     * @return A set of quantified variable symbols.
+     */
+    public final Set<PSymbol> getQuantifiedVariables() {
+        if (myCachedQuantifiedVariables == null) {
+            //We're immutable, so only do this once
+            myCachedQuantifiedVariables =
+                    Collections
+                            .unmodifiableSet(getQuantifiedVariablesNoCache());
+        }
+
+        return myCachedQuantifiedVariables;
+    }
+
+    /**
      * <p>This method returns the list of sub-expressions.</p>
      *
      * @return An immutable list containing {@link PExp} expressions.
      */
     public abstract ImmutableList<PExp> getSubExpressions();
+
+    /**
+     * <p>This method returns an iterator for iterating over the
+     * list of sub-expressions.</p>
+     *
+     * @return An iterator.
+     */
+    public abstract PExpSubexpressionIterator getSubExpressionIterator();
+
+    /**
+     * <p>This method returns a set of symbol names for the current
+     * expression.</p>
+     *
+     * @return A set of names.
+     */
+    public final Set<String> getSymbolNames() {
+        if (myCachedSymbolNames == null) {
+            //We're immutable, so only do this once
+            myCachedSymbolNames =
+                    Collections.unmodifiableSet(getSymbolNamesNoCache());
+        }
+
+        return myCachedSymbolNames;
+    }
+
+    /**
+     * <p>This method returns the top-level operation for the current
+     * expression.</p>
+     *
+     * @return A string representing the top-level operation.
+     */
+    public abstract String getTopLevelOperation();
 
     /**
      * <p>This method overrides the default {@code hashCode} method implementation.</p>
@@ -161,6 +286,22 @@ public abstract class PExp {
     public final int hashCode() {
         return valueHash;
     }
+
+    /**
+     * <p>This method checks to see if this expression is an equality
+     * expression.</p>
+     *
+     * @return {@code true} if it is one, {@code false} otherwise.
+     */
+    public abstract boolean isEquality();
+
+    /**
+     * <p>This method checks to see if this expression is a literal
+     * expression.</p>
+     *
+     * @return {@code true} if it is, {@code false} otherwise.
+     */
+    public abstract boolean isLiteral();
 
     /**
      * <p>This method checks to see if this expression is obviously
@@ -181,6 +322,21 @@ public abstract class PExp {
     public abstract boolean isVariable();
 
     /**
+     * <p>This method processes the current expression using
+     * its string representation.</p>
+     *
+     * @param visitor A visitor for {@link PExp PExps}.
+     * @param a An appendable object.
+     */
+    public final void processStringRepresentation(PExpVisitor visitor,
+            Appendable a) {
+        PExpTextRenderingVisitor renderer = new PExpTextRenderingVisitor(a);
+        PExpVisitor finalVisitor = new NestedPExpVisitors(visitor, renderer);
+
+        this.accept(finalVisitor);
+    }
+
+    /**
      * <p>This method returns a DEEP COPY of this expression, with all instances of
      * {@link PExp PExps} that occur as keys in {@code substitutions}
      * replaced with their corresponding values.</p>
@@ -195,16 +351,131 @@ public abstract class PExp {
     public abstract PExp substitute(Map<PExp, PExp> substitutions);
 
     /**
+     * <p>This method returns the current expression in string format
+     * with proper indentation and offset.</p>
+     *
+     * <p>Note that this method is only used for debugging purposes.</p>
+     *
+     * @param indent The base indentation to the first line
+     *               of the text.
+     * @param offset The additional indentation increment
+     *               for the subsequent lines.
+     *
+     * @return A formatted text string for this {@link PExp}.
+     */
+    public final String toDebugString(int indent, int offset) {
+        StringBuilder b = new StringBuilder();
+        if (this instanceof PSymbol) {
+            b.append(((PSymbol) this).quantification).append(" ");
+        }
+
+        b.append(toString()).append(" : ").append(myMathType);
+
+        if (myMathTypeValue != null) {
+            b.append("(Defines: ").append(myMathTypeValue).append(")");
+        }
+
+        b.append(" ").append(valueHash);
+
+        for (PExp e : getSubExpressions()) {
+            b.append("\n").append(e.toDebugString(indent + offset, offset));
+        }
+
+        return b.toString();
+    }
+
+    /**
+     * <p>This method returns the current expression in string format.</p>
+     *
+     * @return Current {@link PExp} as a string.
+     */
+    public final String toString() {
+        StringWriter output = new StringWriter();
+        PExpTextRenderingVisitor renderer =
+                new PExpTextRenderingVisitor(output);
+
+        this.accept(renderer);
+
+        return output.toString();
+    }
+
+    /**
+     * <p>This static method method creates a variable expression that
+     * matches the boolean {@code true}.</p>
+     *
+     * @param g A {@link TypeGraph} to retrieve the mathematical boolean type.
+     *
+     * @return A {@link PExp} representing {@code true}.
+     */
+    public static PExp trueExp(TypeGraph g) {
+        return new PSymbol(g.BOOLEAN, null, "true");
+    }
+
+    /**
+     * <p>This method returns a new expression by substituting the specified
+     * value in all the sub-expressions.</p>
+     *
+     * @param path A path for iterating over the sub-expressions.
+     * @param newValue New expression value to be substituted.
+     *
+     * @return A modified {@link PExp}.
+     */
+    public final PExp withSiteAltered(Iterator<Integer> path, PExp newValue) {
+        Deque<Integer> integerPath = new LinkedList<>();
+        Deque<PExp> pexpPath = new LinkedList<>();
+
+        pexpPath.push(this);
+        while (path.hasNext()) {
+            integerPath.push(path.next());
+
+            pexpPath.push(pexpPath.peek().getSubExpressions().get(
+                    integerPath.peek()));
+        }
+
+        pexpPath.pop();
+        pexpPath.push(newValue);
+
+        while (pexpPath.size() > 1) {
+            newValue = pexpPath.pop();
+            pexpPath.push(pexpPath.pop().withSubExpressionReplaced(
+                    integerPath.pop(), newValue));
+        }
+
+        return pexpPath.peek();
+    }
+
+    /**
      * <p>This method attempts to replace an argument at the specified
      * index.</p>
      *
-     * @param index Index to an argument.
+     * @param index Index to an expression argument.
      * @param e The {@link PExp} to replace the one in our argument list.
      *
      * @return A new {@link PExp} with the expression at the specified index
      * replaced with {@code e}.
      */
     public abstract PExp withSubExpressionReplaced(int index, PExp e);
+
+    /**
+     * <p>This method provides a mapping of expressions that needs to be
+     * replaced at each index key.</p>
+     *
+     * @param e A mapping from index to some expression argument.
+     *
+     * @return A new {@link PExp} with the expression replaced by all
+     * the key-value pairs in {@code e}.
+     */
+    public final PExp withSubExpressionsReplaced(Map<Integer, PExp> e) {
+        PExp working = this;
+
+        for (Map.Entry<Integer, PExp> entry : e.entrySet()) {
+            working =
+                    working.withSubExpressionReplaced(entry.getKey(), entry
+                            .getValue());
+        }
+
+        return working;
+    }
 
     /**
      * <p>This method returns a new expression with the mathematical type
@@ -230,9 +501,81 @@ public abstract class PExp {
     // Protected Methods
     // ===========================================================
 
+    /**
+     * <p>This method returns a list of sub-expressions that are function
+     * applications in the current expression without using the cache.</p>
+     *
+     * @return A list of sub-expressions that are function applications.
+     */
+    protected abstract List<PExp> getFunctionApplicationsNoCache();
+
+    /**
+     * <p>This method returns a set of quantified variables without using
+     * the cache.</p>
+     *
+     * @return A set of quantified variables.
+     */
+    protected abstract Set<PSymbol> getQuantifiedVariablesNoCache();
+
+    /**
+     * <p>This method returns a set of symbol names without using
+     * the cache.</p>
+     *
+     * @return A set of names.
+     */
+    protected abstract Set<String> getSymbolNamesNoCache();
+
+    /**
+     * <p>This method checks to see if the specified type
+     * matches the current expression's mathematical type.</p>
+     *
+     * @param other Some other mathematical type.
+     *
+     * @return {@code true} if it matches, {@code false} otherwise.
+     */
+    protected final boolean typeMatches(MTType other) {
+        return other.isSubtypeOf(myMathType);
+    }
+
+    /**
+     * <p>This method checks to see if the specified expression
+     * has a type that matches the current expression's mathematical type.</p>
+     *
+     * @param other Some other {@link PExp}.
+     *
+     * @return {@code true} if it matches, {@code false} otherwise.
+     */
+    protected final boolean typeMatches(PExp other) {
+        return typeMatches(other.getMathType());
+    }
+
     // ===========================================================
     // Private Methods
     // ===========================================================
+
+    /**
+     * <p>An helper method for generating the full name with the qualifier.</p>
+     *
+     * @param qualifier A qualifier name.
+     * @param name A name.
+     *
+     * @return A string with the qualifier name (if not {@code null}) prepended
+     * before the specified name.
+     */
+    private static String fullName(PosSymbol qualifier, String name) {
+        StringBuilder sb = new StringBuilder();
+
+        if (qualifier != null) {
+            if (qualifier.getName() != null) {
+                sb.append(qualifier.getName());
+                sb.append(".");
+            }
+        }
+
+        sb.append(name);
+
+        return sb.toString();
+    }
 
     // ===========================================================
     // Helper Constructs
