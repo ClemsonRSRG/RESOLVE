5fe5b85d32e972bb61673b86bec208e26e830adf
JL <koenigjl@rose-hulman.edu>
2024-12-04 14:13:26 -0500

    #16 added test case for retrieveWarningCount with on warning

--- test/java/edu/clemson/rsrg/statushandling/WriterStatusHandlerTest.java
+++ test/java/edu/clemson/rsrg/statushandling/WriterStatusHandlerTest.java
@@ -1,5 +1,6 @@
 package edu.clemson.rsrg.statushandling;
 
+import edu.clemson.rsrg.parsing.data.Location;
 import org.junit.Test;
 
 import java.io.PrintWriter;
@@ -10,5 +11,11 @@ public class WriterStatusHandlerTest {
     public void testRetrieveWarningCount_givenNoWarnings_returnZero() {
         StatusHandler testHandler = new WriterStatusHandler(new PrintWriter(System.out), new PrintWriter(System.err));
         assert(testHandler.retrieveWarningCount() == 0);
+    }
+
+    @Test
+    public void testRetrieveWarningCount_givenSingleWarning_returnOne() {
+        StatusHandler testHandler = new WriterStatusHandler(new PrintWriter(System.out), new PrintWriter(System.err));
+        testHandler.warning(new Location(null, 12 ,12), "test single error");
     }
 }
