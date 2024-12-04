367d36967ea8ef50cff7968cb137294789e1be0f
William Foss <fosswe@rose-hulman.edu>
2024-12-04 13:34:38 -0500

    File outline.

---
+++ src/java/edu/clemson/rsrg/parsing/utilities/WarningHandler.java
@@ -0,0 +1,25 @@
+package edu.clemson.rsrg.parsing.utilities;
+
+public class WarningHandler {
+
+    public WarningHandler() {
+
+
+    }
+
+//      Collection of warnings (List?)
+//          Warnings have Location (take this from the existing class)
+//          Enum for WarningType
+//          Plaintext description / Error info
+
+//      Subclass WarningIterator
+
+//      Method createWarning
+//      Method retrieveSingleWarning
+//      Method retrieveWarnings
+
+//      In Control.java, we'll have a flag that allows to export warnings to a separate text file
+//          By default we print to console
+//          We'll need to export probably using FileOutputListener (maybe)
+
+}
