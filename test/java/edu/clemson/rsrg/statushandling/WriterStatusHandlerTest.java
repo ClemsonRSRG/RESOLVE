43ddf5b795898a8913ae447508f73e115db43149
JL <koenigjl@rose-hulman.edu>
2024-12-10 16:28:02 -0500

    #16 updated tests using mockito to work

--- test/java/edu/clemson/rsrg/statushandling/WriterStatusHandlerTest.java
+++ test/java/edu/clemson/rsrg/statushandling/WriterStatusHandlerTest.java
@@ -1,33 +1,61 @@
+/*
+ * WriterStatusHandlerTest.java
+ * ---------------------------------
+ * Copyright (c) 2024
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
 package edu.clemson.rsrg.statushandling;
 
+import edu.clemson.rsrg.init.file.ModuleType;
+import edu.clemson.rsrg.init.file.ResolveFile;
+import edu.clemson.rsrg.init.file.ResolveFileBasicInfo;
 import edu.clemson.rsrg.parsing.data.Location;
 import org.junit.Test;
 
 import java.io.PrintWriter;
 import java.io.Writer;
+import static org.mockito.Mockito.*;
 
 public class WriterStatusHandlerTest {
     @Test
     public void testRetrieveWarningCount_givenNoWarnings_returnZero() {
         StatusHandler testHandler = new WriterStatusHandler(new PrintWriter(System.out), new PrintWriter(System.err));
-        assert(testHandler.retrieveWarningCount() == 0);
+        assert (testHandler.retrieveWarningCount() == 0);
     }
 
     @Test
     public void testRetrieveWarningCount_givenSingleWarning_returnOne() {
         StatusHandler testHandler = new WriterStatusHandler(new PrintWriter(System.out), new PrintWriter(System.err));
-        testHandler.warning(new Location(null, 12 ,12), "test single error");
-        assert(testHandler.retrieveWarningCount() == 1);
+
+        ResolveFile rsFile = mock(ResolveFile.class);
+        when(rsFile.getModuleType()).thenReturn(ModuleType.THEORY);
+
+        Location warningLocation = new Location(rsFile, 12, 12);
+
+        testHandler.warning(warningLocation, "test single error");
+        assert (testHandler.retrieveWarningCount() == 1);
     }
 
     @Test
     public void testRetrieveWarningCount_givenMultipleWarnings_returns5() {
         StatusHandler testHandler = new WriterStatusHandler(new PrintWriter(System.out), new PrintWriter(System.err));
-        testHandler.warning(new Location(null, 12 ,12), "test single error");
-        testHandler.warning(new Location(null, 12 ,12), "test single error");
-        testHandler.warning(new Location(null, 12 ,12), "test single error");
-        testHandler.warning(new Location(null, 12 ,12), "test single error");
-        testHandler.warning(new Location(null, 12 ,12), "test single error");
-        assert(testHandler.retrieveWarningCount() == 5);
+
+        ResolveFile rsFile = mock(ResolveFile.class);
+        when(rsFile.getModuleType()).thenReturn(ModuleType.THEORY);
+
+        Location warningLocation = new Location(rsFile, 12, 12);
+
+        testHandler.warning(warningLocation, "test single error");
+        testHandler.warning(warningLocation, "test single error");
+        testHandler.warning(warningLocation, "test single error");
+        testHandler.warning(warningLocation, "test single error");
+        testHandler.warning(warningLocation, "test single error");
+        assert (testHandler.retrieveWarningCount() == 5);
     }
 }
