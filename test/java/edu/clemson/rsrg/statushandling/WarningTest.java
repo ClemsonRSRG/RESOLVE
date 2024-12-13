76ae6899b5375c511a61ce4ed1cf61f0cc2677a3
JL <koenigjl@rose-hulman.edu>
2024-12-12 21:17:35 -0500

    #16 updated old tests for new constructor

--- test/java/edu/clemson/rsrg/statushandling/WarningTest.java
+++ test/java/edu/clemson/rsrg/statushandling/WarningTest.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 public class WarningTest {
     @Test
     public void testWarningIsType_givenGenericType_returnTrue() {
-        Warning warning = new Warning(WarningType.GENERIC_WARNING);
+        Warning warning = new Warning(WarningType.GENERIC_WARNING, "");
 
         assert(warning.isType(WarningType.GENERIC_WARNING));
     }
 
     @Test
     public void testWarningIsType_givenIncorrectParameterModeUsage_returnTrue() {
-        Warning warning = new Warning(WarningType.INCORRECT_PARAMETER_MODE_USAGE);
+        Warning warning = new Warning(WarningType.INCORRECT_PARAMETER_MODE_USAGE, "");
 
         assert(warning.isType(WarningType.INCORRECT_PARAMETER_MODE_USAGE));
     }
 
     @Test
     public void testWarningGetMessage_givenGenericString_returnsGenericString() {
-        Warning warning = new Warning(WarningType.GENERIC_WARNING);
+        Warning warning = new Warning(WarningType.GENERIC_WARNING, "abcd");
 
         assert(warning.getMessage().equals("abcd"));
     }
