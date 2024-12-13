66318632458472fec35b2e711dc49b7a266665c2
JL <koenigjl@rose-hulman.edu>
2024-12-13 14:27:37 -0500

    #16 added registerAndStreamWarning method that will factor out existing warning method

--- src/java/edu/clemson/rsrg/statushandling/StatusHandler.java
+++ src/java/edu/clemson/rsrg/statushandling/StatusHandler.java
@@ -96,6 +96,16 @@ public interface StatusHandler {
 
     /**
      * <p>
+     * This method registers and displays compiler warning passed in.
+     * </p>
+     *
+     * @param warning
+     *          The warning to be registered and displayed
+     */
+    void registerAndStreamWarning(Warning warning);
+
+    /**
+     * <p>
      * This method returns the number of warnings captured by this status handler.
      * </p>
      * @return
