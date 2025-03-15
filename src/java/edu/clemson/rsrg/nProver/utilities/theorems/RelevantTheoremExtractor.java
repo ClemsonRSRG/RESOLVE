d4917221aea36fc66f260fb1c2c1b95e25984dff
AdityaSenthilvel <senthia1@rose-hulman.edu>
2025-03-15 17:41:25 -0400

    updated prover files

--- src/java/edu/clemson/rsrg/nProver/utilities/theorems/RelevantTheoremExtractor.java
+++ src/java/edu/clemson/rsrg/nProver/utilities/theorems/RelevantTheoremExtractor.java
@@ -12,36 +12,73 @@
  */
 package edu.clemson.rsrg.nProver.utilities.theorems;
 
+import edu.clemson.rsrg.absyn.expressions.Exp;
+import edu.clemson.rsrg.absyn.expressions.mathexpr.VarExp;
+import edu.clemson.rsrg.prover.utilities.theorems.Theorem;
 import edu.clemson.rsrg.typeandpopulate.entry.TheoremEntry;
 import edu.clemson.rsrg.typeandpopulate.query.EntryTypeQuery;
 import edu.clemson.rsrg.typeandpopulate.symboltables.MathSymbolTable;
 import edu.clemson.rsrg.typeandpopulate.symboltables.ModuleScope;
 import edu.clemson.rsrg.typeandpopulate.utilities.ModuleIdentifier;
+import edu.clemson.rsrg.vcgeneration.sequents.Sequent;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class RelevantTheoremExtractor {
 
     private final ModuleScope myCurrentModuleScope;
 
+    private List<TheoremEntry> programTheorems = null;
+
     public RelevantTheoremExtractor(ModuleScope scope) {
         myCurrentModuleScope = scope;
     }
 
-    public List<TheoremEntry> theoremEntryQuery() {
-        List<TheoremEntry> te = null;
-        te = myCurrentModuleScope.query(new EntryTypeQuery<TheoremEntry>(TheoremEntry.class,
+    // this query should happen once as the collection is for the entire program. The design is just for performance
+    public void theoremEntryQuery() {
+        // ist<TheoremEntry> te = null;
+        programTheorems = myCurrentModuleScope.query(new EntryTypeQuery<TheoremEntry>(TheoremEntry.class,
                 MathSymbolTable.ImportStrategy.IMPORT_NAMED, MathSymbolTable.FacilityStrategy.FACILITY_IGNORE));
-        System.err.println(te.size());
+        System.err.println(programTheorems.size());
+    }
 
-        Set<ModuleIdentifier> sett = new HashSet<>();
+    // get all those theorems including all operators and constants on the sequent VC to be proved
+    public List<TheoremEntry> getSequentVCTheorems(Map<String, Integer> expLabels) {
 
-        for (TheoremEntry theoremEntry : te) {
-            sett.add(theoremEntry.getSourceModuleIdentifier());
+        // set of relevant theorems to the sequent VC
+        List<TheoremEntry> relevantTheorems = new ArrayList<>();
+        // set of theorem operators
+        Set<Exp> setOfTheoremOperators;
+
+        List<String> vcOperators = new ArrayList<>();
+
+        vcOperators.addAll(expLabels.keySet());
+        Boolean fullyContained = false;
+
+        // check theorem operators if they are in the VC, all theorem operators have to be in the VC operators
+        for (TheoremEntry theoremEntry : programTheorems) {
+            // System.out.println(theoremEntry.getAssertion());
+            setOfTheoremOperators = theoremEntry.getOperators();
+
+            // for each operator in the theoremEntry
+            for (Exp e : setOfTheoremOperators) {
+                // check to see if the operator e is in the sequent VC operator list
+                if (vcOperators.contains(e.toString())) {
+                    fullyContained = true;
+                } else {
+                    fullyContained = false;
+                    break;
+                }
+            }
+            // add the theoremEntry to the list
+            if (fullyContained) {
+                relevantTheorems.add(theoremEntry);
+            }
+            // just to be sure nothing is left in the set
+            setOfTheoremOperators.clear();
         }
-        System.out.println(sett);
-        return te;
+        System.out.print("Number of relevant theorems: ");
+        System.out.println(relevantTheorems.size());
+        return relevantTheorems;
     }
 }
