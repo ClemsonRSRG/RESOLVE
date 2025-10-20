6f7d33527cd9623ef64cdf5190d5eb64ad541a92
Carson Holscher <holschcc@rose-hulman.edu>
2025-10-20 18:38:55 -0400

    Also ran license:format

--- src/java/edu/clemson/rsrg/nProver/utilities/theorems/TheoremStore.java
+++ src/java/edu/clemson/rsrg/nProver/utilities/theorems/TheoremStore.java
@@ -1,3 +1,15 @@
+/*
+ * TheoremStore.java
+ * ---------------------------------
+ * Copyright (c) 2024
+ * RESOLVE Software Research Group
+ * School of Computing
+ * Clemson University
+ * All rights reserved.
+ * ---------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
 package edu.clemson.rsrg.nProver.utilities.theorems;
 
 import edu.clemson.rsrg.absyn.expressions.Exp;
